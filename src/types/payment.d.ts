2fae0ffd923a80f87f74e79ef56e13d81812b178
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-17 22:33:33 +0800

    Add proofImage and proofImageUrl for Payment type

--- src/types/payment.d.ts
+++ src/types/payment.d.ts
@@ -4,6 +4,8 @@ export interface Payment {
   _id?: ObjectId
   amount?: number
   method?: string
+  proofImage?: File
+  proofImageUrl?: string
   status?: string
   createdAt?: Date
   updatedAt?: Date
@@ -13,12 +15,14 @@ export interface CreatePaymentArgs {
   _id?: ObjectId
   amount: number
   method: string
+  proofImage?: File
   status?: string
   createdAt?: Date
 }
 
 export interface UpdatePaymentArgs {
   _id?: ObjectId
+  proofImage?: File
   status?: string
   updatedAt?: Date
 }
