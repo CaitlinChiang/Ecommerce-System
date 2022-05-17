b382a88e2ea33db529a489475bb943c6af0ce783
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-17 19:33:40 +0800

    Create Payment type

--- src/types/payment.d.ts
+++ src/types/payment.d.ts
@@ -1,2 +1,23 @@
 import { ObjectId } from "mongodb";
 
+export interface Payment {
+  _id?: ObjectId
+  amount?: number
+  method?: string
+  status?: string
+  createdAt?: Date
+}
+
+export interface CreatePaymentArgs {
+  _id?: ObjectId
+  amount: number
+  method: string
+  status?: string
+  createdAt?: Date
+}
+
+export interface UpdatePaymentArgs {
+  _id?: ObjectId
+  status?: string
+  updatedAt?: Date
+}
