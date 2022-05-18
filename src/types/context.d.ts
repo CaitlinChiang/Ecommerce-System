cb6395ba3309dc2295be3f9a123afd622da79fbb
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 15:29:00 +0800

    Install dotenv

--- src/types/context.d.ts
+++ src/types/context.d.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'mongodb'
 import { Database } from './database'
 
 export interface Context {
+  ip?: string | string[]
   currentUserId?: ObjectId
   database?: Database
 }
