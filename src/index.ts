0b2996f29494130ee74e0da362c8fcc7eb2bb599
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 14:47:12 +0800

    Create dbSetup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import 'graphql-import-node'
-
+import {
+  ApolloServer,
+  AuthenticationError,
+  ForbiddenError,
+  UserInputError,
+  ApolloError
+} from 'apollo-server-express'
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
+import next from 'next'
+import { parse } from 'url'
+import _dbSetup from './backend/_utils/dbSetup'
