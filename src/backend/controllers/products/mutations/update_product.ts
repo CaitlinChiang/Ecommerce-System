e556cc8e3c00068e29584c96e6f5440d3f93b35e
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 21:54:51 +0800

    Update product mutation

---
+++ src/backend/controllers/products/mutations/update_product.ts
@@ -0,0 +1,27 @@
+import { Context } from 'types/context'
+import { Product, UpdateProductArgs } from 'types/product'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: UpdateProductArgs,
+  context: Context
+): Promise<Product> => {
+  const updateProduct: Partial<UpdateProductArgs> = {
+    name: args?.name,
+    price: args?.price,
+    type: args?.type,
+    updatedAt: new Date()
+  }
+
+  const product: any = await context.database.products.findOneAndUpdate({ _id: args._id }, updateProduct)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.UPDATE_PRODUCT,
+    productId: product._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  return product
+}
