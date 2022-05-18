1d804eb7826875dbf1707395d9caba0a7a8e5457
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 21:55:06 +0800

    Create delete_product mutation

---
+++ src/backend/controllers/products/mutations/delete_product.ts
@@ -0,0 +1,20 @@
+import { Context } from 'types/context'
+import { Product, DeleteProductArgs } from 'types/product'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: DeleteProductArgs,
+  context: Context
+): Promise<Product> => {
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.DELETE_PRODUCT,
+    productId: args._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  const product: any = await context.database.products.findOneAndDelete({ _id: args._id })
+
+  return product
+}
