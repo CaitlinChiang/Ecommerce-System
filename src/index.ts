57cb57e2cb149994baa671685f44e7723f34cd1c
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 16:13:41 +0800

    Initial setup of server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,68 @@
 import 'graphql-import-node'
+import * as dotenv from 'dotenv'
+import * as mongoDB from 'mongodb'
 import {
   ApolloServer,
   AuthenticationError,
   ForbiddenError,
   UserInputError,
-  ApolloError
+  ExpressContext
 } from 'apollo-server-express'
-import compression from 'compression'
+import { GraphQLFormattedError, GraphQLError } from 'graphql'
 import cors from 'cors'
 import express from 'express'
 import next from 'next'
 import { parse } from 'url'
-import _dbSetup from './backend/_utils/dbSetup'
+import dbSetup from './backend/_utils/dbSetup'
+import { Context } from './types/context'
+import { Database } from './types/database'
+import { resolvers, typeDefs } from './backend/controllers'
+
+const app = express()
+      app.set('trust proxy', true)
+      app.use(cors())
+
+const dev = process.env.NODE_ENV !== 'production'
+const nextJSApp = next({ dir: './src/frontend', dev })
+const handle = nextJSApp.getRequestHandler()
+
+nextJSApp.prepare().then(async () => {
+  dotenv.config()
+  
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONNECTION_STRING)
+  await client.connect()
+  const db: mongoDB.Db = client.db(process.env.DB_NAME)
+  const database: Database = dbSetup(db)
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async (context: ExpressContext): Promise<Context> => {
+      const ip = context.req.headers['CF-Connecting-IP'] || context.req.headers['X-Forwarded-For'] || context.req.ip
+      return { database, ip }
+    },
+    formatError: (error: GraphQLError): GraphQLFormattedError => {
+      if (
+        !(
+          error.originalError instanceof AuthenticationError ||
+          error.originalError instanceof ForbiddenError ||
+          error.originalError instanceof UserInputError
+        )
+      ) {
+        console.error(error)
+      }
+      return error
+    }
+  })
+
+  server.applyMiddleware({ app, path: '/graphql' })
+
+  app.use((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  })
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server Ready on port ${process.env.PORT || 3000}`)
+  })
+})
