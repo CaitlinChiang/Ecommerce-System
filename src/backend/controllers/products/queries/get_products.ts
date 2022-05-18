de1254f020765c8bd3dc4b1300d12850b511ab06
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-18 22:27:19 +0800

    Create get_products query

---
+++ src/backend/controllers/products/queries/get_products.ts
@@ -0,0 +1,11 @@
+import { Context } from 'types/context'
+import { Product } from 'types/product'
+
+export default async (
+  _root: undefined,
+  args: undefined,
+  context: Context
+): Promise<Product[]> => {
+  const products: any = await context.database.products.find({})
+  return products
+}
