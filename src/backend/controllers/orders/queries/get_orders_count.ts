3163b42034caf07fd3d1a9979d8487d07d4e2884
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 16:38:32 +0800

    Create get_orders_count query

---
+++ src/backend/controllers/orders/queries/get_orders_count.ts
@@ -0,0 +1,11 @@
+import { Context } from 'types/context'
+import { Order } from 'types/order'
+
+export default async (
+  _root: undefined,
+  args: undefined,
+  context: Context
+): Promise<number> => {
+  const ordersCount: number = await context.database.orders.countDocuments()
+  return ordersCount
+}
---
+++ src/backend/controllers/orders/queries/index.ts
@@ -0,0 +1,9 @@
+import get_order from './get_order'
+import get_orders from './get_orders'
+import get_orders_count from './get_orders_count'
+
+export default {
+  get_order,
+  get_orders,
+  get_orders_count
+}
