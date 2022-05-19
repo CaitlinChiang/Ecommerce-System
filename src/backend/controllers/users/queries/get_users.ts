b7d10271173f9429416ce432b6ec64a60c370fef
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 14:23:27 +0800

    Create get_users query

---
+++ src/backend/controllers/users/queries/get_users.ts
@@ -0,0 +1,11 @@
+import { Context } from 'types/context'
+import { User } from 'types/user'
+
+export default async (
+  _root: undefined,
+  args: undefined,
+  context: Context
+): Promise<User[]> => {
+  const users: any = await context.database.users.find({})
+  return users
+}
