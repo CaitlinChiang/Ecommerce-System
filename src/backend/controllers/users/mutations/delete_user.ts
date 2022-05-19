610e514eab0f7432efc4f669467ee07320047efb
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 13:50:44 +0800

    Create delete_user mutation

---
+++ src/backend/controllers/users/mutations/delete_user.ts
@@ -0,0 +1,19 @@
+import { Context } from 'types/context'
+import { User, DeleteUserArgs } from 'types/user'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: DeleteUserArgs,
+  context: Context
+): Promise<User> => {
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.DELETE_USER,
+    userId: args._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  const user: any = await context.database.users.findOneAndDelete({ _id: args._id })
+  return user
+}
---
+++ src/backend/controllers/users/mutations/index.ts
@@ -0,0 +1,9 @@
+import create_user from './create_user'
+import update_user from './update_user'
+import delete_user from './delete_user'
+
+export default {
+  create_user,
+  update_user,
+  delete_user
+}
