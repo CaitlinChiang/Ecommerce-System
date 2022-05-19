8ff99cf83c30ad216a49c64e3b702c15c7c72d8d
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 16:38:26 +0800

    Create get_orders query

---
+++ src/backend/controllers/orders/queries/get_orders.ts
@@ -0,0 +1,11 @@
+import { Context } from 'types/context'
+import { Order } from 'types/order'
+
+export default async (
+  _root: undefined,
+  args: undefined,
+  context: Context
+): Promise<Order[]> => {
+  const orders: any = await context.database.orders.find({})
+  return orders
+}
