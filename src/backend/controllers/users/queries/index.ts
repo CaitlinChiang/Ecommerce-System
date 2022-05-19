1fad5e7bd3166f3fbd10475a6fb54393365df7d4
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 13:59:22 +0800

    Export user queries

---
+++ src/backend/controllers/users/queries/index.ts
@@ -0,0 +1,9 @@
+import get_user from './get_user'
+import get_users from './get_users'
+import get_users_count from './get_users_count'
+
+export default {
+  get_user,
+  get_users,
+  get_users_count
+}
