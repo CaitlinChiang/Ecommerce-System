104483f578b4d2f12abe7f51cb77979956a37e0f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 13:40:30 +0800

    Create create_user mutation

---
+++ src/backend/controllers/users/mutations/create_user.ts
@@ -0,0 +1,31 @@
+import { Context } from 'types/context'
+import { User, CreateUserArgs } from 'types/user'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: CreateUserArgs,
+  context: Context
+): Promise<User> => {
+  const createUser: CreateUserArgs = {
+    address: args?.address,
+    email: args.email,
+    firstName: args.firstName,
+    lastName: args.lastName,
+    password: args.password,
+    phoneNumber: args.phoneNumber,
+    type: args.type,
+    createdAt: new Date()
+  }
+
+  const user: any = await context.database.users.insertOne(createUser)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.CREATE_USER,
+    productId: user._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  return user
+}
