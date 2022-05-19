bd7ac461dbba4a166279f1ac088480b368234941
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 18:50:28 +0800

    Add update_payment to gql typDefs

--- src/backend/controllers/orders/mutations/index.ts
+++ src/backend/controllers/orders/mutations/index.ts
@@ -1,9 +1,11 @@
 import create_order from './create_order'
 import update_order from './update_order'
+import update_payment from './update_payment'
 import delete_order from './delete_order'
 
 export default {
   create_order,
   update_order,
+  update_payment,
   delete_order
 }
