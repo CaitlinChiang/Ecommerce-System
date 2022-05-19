dbd4c09402cd3183ab5d55388b71da3fbe135c24
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 12:58:08 +0800

    Create create_product_variant mutation

---
+++ src/backend/controllers/productVariants/mutations/create_product_variant.ts
@@ -0,0 +1,27 @@
+import { Context } from 'types/context'
+import { ProductVariant, CreateProductVariantArgs } from 'types/productVariant'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: CreateProductVariantArgs,
+  context: Context
+): Promise<ProductVariant> => {
+  const createProductVariant: CreateProductVariantArgs = {
+    _productId: args._productId,
+    name: args.name,
+    price: args.price,
+    createdAt: new Date()
+  }
+
+  const productVariant: any = await context.database.productVariants.insertOne(createProductVariant)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.CREATE_PRODUCT_VARIANT,
+    productId: productVariant._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  return productVariant
+}
