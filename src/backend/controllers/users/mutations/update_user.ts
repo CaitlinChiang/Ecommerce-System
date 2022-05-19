d878ca5dde2ac1fe2dff783f9e27b352e229b742
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-19 13:44:24 +0800

    Create update_user mutation

---
+++ src/backend/controllers/users/mutations/update_user.ts
@@ -0,0 +1,28 @@
+import { Context } from 'types/context'
+import { User, UpdateUserArgs } from 'types/user'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: UpdateUserArgs,
+  context: Context
+): Promise<User> => {
+  const updateUser: Partial<UpdateUserArgs> = {
+    address: args?.address,
+    email: args.email,
+    password: args.password,
+    phoneNumber: args.phoneNumber,
+    updatedAt: new Date()
+  }
+
+  const user: any = await context.database.users.findOneAndUpdate({ _id: args._id }, updateUser)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.UPDATE_USER,
+    userId: user._id,
+    createdBy: context.currentUserId,
+    createdAt: new Date()
+  })
+
+  return user
+}
