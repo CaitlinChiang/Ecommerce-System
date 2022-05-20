e72637294cbe374fa10f89032f5a555bd47e1187
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-20 21:51:22 +0800

    Add carts database

--- src/backend/_utils/dbSetup.ts
+++ src/backend/_utils/dbSetup.ts
@@ -2,6 +2,7 @@ import { Db } from 'mongodb'
 import { Database } from 'types/database'
 
 import { AuditLog } from 'types/auditLog'
+import { Cart } from 'types/cart'
 import { Order } from 'types/order'
 import { Payment } from 'types/payment'
 import { Product } from 'types/product'
@@ -11,6 +12,7 @@ import { User } from 'types/user'
 export default (db: Db): Database => {
   return {
     auditLogs: db.collection<AuditLog>('auditLogs'),
+    carts: db.collection<Cart>('carts'),
     orders: db.collection<Order>('orders'),
     payments: db.collection<Payment>('payments'),
     products: db.collection<Product>('products'),
--- src/types/database.d.ts
+++ src/types/database.d.ts
@@ -1,5 +1,6 @@
 import { Collection } from 'mongodb'
 import { AuditLog } from './auditLog'
+import { Cart } from './cart'
 import { Order } from './order'
 import { Payment } from './payment'
 import { Product } from './product'
@@ -8,6 +9,7 @@ import { User } from './user'
 
 export interface Database {
   auditLogs: Collection<AuditLog>
+  carts: Collection<Cart>
   orders: Collection<Order>
   payments: Collection<Payment>
   products: Collection<Product>
