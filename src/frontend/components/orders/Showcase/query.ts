91cff6d2673c1d02f75d3aaf42230f6bc9484f9c
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-21 20:39:54 +0800

    Create orders query in the frontend component

---
+++ src/frontend/components/orders/Showcase/query.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export default gql`
+  query(
+    $_id: ID!
+  ) {
+    get_order(
+      _id: $_id
+    ) {
+      _id
+    }
+
+    get_orders {
+      _id
+    }
+
+    get_orders_count
+  }
+`
