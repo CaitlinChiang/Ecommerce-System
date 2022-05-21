81ff336f6fc26b3746de1c5dec2d3bbb816c661f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-21 18:44:29 +0800

    Create productVariants query in frontend component

---
+++ src/frontend/components/productVariants/Showcase/query.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export default gql`
+  query(
+    $_id: ID!
+  ) {
+    get_product_variant(
+      _id: $_id
+    ) {
+      _id
+    }
+
+    get_product_variants {
+      _id
+    }
+
+    get_product_variants_count
+  }
+`
