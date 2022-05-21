febb19cb89b44441dc10102e59977ae74c4a47ed
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-21 18:35:56 +0800

    Create create_mutation to frontend component

---
+++ src/frontend/components/users/Create/mutation.ts
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client'
+
+export default gql`
+  mutation(
+    $address: String
+    $email: String!
+    $firstName: String!
+    $lastName: String!
+    $password: String!
+    $phoneNumber: String!
+    $type: String!
+  ) {
+    create_user(
+      address: $address
+      email: $email
+      firstName: $firstName
+      lastName: $lastName
+      password: $password
+      phoneNumber: $phoneNumber
+      type: $type
+    ) {
+      _id
+    }
+  }
+`
