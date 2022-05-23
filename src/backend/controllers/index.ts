33ff75e85f68656a18de60ec6605d6ed872ed1af
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-23 14:10:43 +0800

    Add cart to exportation

--- src/backend/controllers/index.ts
+++ src/backend/controllers/index.ts
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-server-express'
 import auditLogs from './auditLogs'
+import cart from './cart'
 import orders from './orders'
 import products from './products'
 import productVariants from './productVariants'
@@ -11,12 +12,14 @@ const emptyDefs = gql`
 `
 
 export const resolvers = [
+  cart.resolvers,
   orders.resolvers
 ]
 
 export const typeDefs = [
   emptyDefs,
   auditLogs.typeDefs,
+  cart.typeDefs,
   orders.typeDefs,
   products.typeDefs,
   productVariants.typeDefs,
