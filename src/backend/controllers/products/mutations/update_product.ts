6daaf4652b8d99cf9fb53e819f9c3c289555f1d3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-25 21:39:52 +0800

    Image logic for update product

--- src/backend/controllers/products/mutations/update_product.ts
+++ src/backend/controllers/products/mutations/update_product.ts
@@ -1,21 +1,33 @@
 import { Context } from 'types/context'
 import { Product, UpdateProductArgs } from 'types/product'
+import { UploadImageArgs } from 'types/image'
+import { UploadImageType } from 'types/_enums/uploadImageType'
 import { AuditLogAction } from 'types/_enums/auditLogAction'
+import { handleUploadImage } from 'backend/_utils/handleImages/uploadImage'
+import { handleDeleteImage } from 'backend/_utils/handleImages/deleteImage'
 
 export default async (
   _root: undefined,
   args: UpdateProductArgs,
   context: Context
 ): Promise<Product> => {
-  const { _id, name, price, type } = args
+  const { _id, image, imageUrl, name, price, type } = args
+
+  await handleDeleteImage(imageUrl)
+  const uploadImage: UploadImageArgs = {
+    imageType: UploadImageType.PRODUCT,
+    image: image,
+    productName: name
+  }
+  const modifiedImageUrl = await handleUploadImage(uploadImage)
   
   const updateProduct: Partial<UpdateProductArgs> = {
+    imageUrl: modifiedImageUrl,
     name: name,
     price: price,
     type: type,
     updatedAt: new Date()
   }
-
   const product: any = await context.database.products.findOneAndUpdate({ _id: _id }, updateProduct)
 
   await context.database.auditLogs.insertOne({
