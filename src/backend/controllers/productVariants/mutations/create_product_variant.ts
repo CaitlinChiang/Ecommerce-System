94bb56bbc2ef101ccafddce8fb818ffe53639a4a
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-25 22:06:42 +0800

    Create photo uploading logic for creating product variant

--- src/backend/controllers/productVariants/mutations/create_product_variant.ts
+++ src/backend/controllers/productVariants/mutations/create_product_variant.ts
@@ -1,21 +1,32 @@
 import { Context } from 'types/context'
 import { ProductVariant, CreateProductVariantArgs } from 'types/productVariant'
+import { UploadImageArgs } from 'types/image'
+import { UploadImageType } from 'types/_enums/uploadImageType'
 import { AuditLogAction } from 'types/_enums/auditLogAction'
+import { handleUploadImage } from 'backend/_utils/handleImages/uploadImage'
 
 export default async (
   _root: undefined,
   args: CreateProductVariantArgs,
   context: Context
 ): Promise<ProductVariant> => {
-  const { _productId, name, price } = args
+  const { _productId, image, name, price } = args
+
+  const uploadImage: UploadImageArgs = {
+    imageType: UploadImageType.PRODUCT_VARIANT,
+    image: image,
+    productId: String(_productId),
+    productVariantName: name
+  }
+  const imageUrl = await handleUploadImage(uploadImage)
 
   const createProductVariant: CreateProductVariantArgs = {
     _productId: _productId,
+    imageUrl: imageUrl,
     name: name,
     price: price,
     createdAt: new Date()
   }
-
   const productVariant: any = await context.database.productVariants.insertOne(createProductVariant)
 
   await context.database.auditLogs.insertOne({
