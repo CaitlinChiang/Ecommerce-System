c7269428f072961f180e7d926624644a9f28f3a3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-25 21:39:44 +0800

    Image logic for create product

--- src/backend/controllers/products/mutations/create_product.ts
+++ src/backend/controllers/products/mutations/create_product.ts
@@ -1,21 +1,31 @@
 import { Context } from 'types/context'
 import { Product, CreateProductArgs } from 'types/product'
+import { UploadImageArgs } from 'types/image'
+import { UploadImageType } from 'types/_enums/uploadImageType'
 import { AuditLogAction } from 'types/_enums/auditLogAction'
+import { handleUploadImage } from 'backend/_utils/handleImages/uploadImage'
 
 export default async (
   _root: undefined,
   args: CreateProductArgs,
   context: Context
 ): Promise<Product> => {
-  const { name, price, type } = args
+  const { image, name, price, type } = args
+
+  const uploadImage: UploadImageArgs = {
+    imageType: UploadImageType.PRODUCT,
+    image: image,
+    productName: name
+  }
+  const imageUrl = await handleUploadImage(uploadImage)
 
   const createProduct: CreateProductArgs = {
+    imageUrl: imageUrl,
     name: name,
     price: price,
     type: type,
     createdAt: new Date()
   }
-
   const product: any = await context.database.products.insertOne(createProduct)
 
   await context.database.auditLogs.insertOne({
