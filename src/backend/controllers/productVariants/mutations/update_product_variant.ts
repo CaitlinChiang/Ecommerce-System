53c9ef0aff2f25990592d387bac0f20767a4ce23
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-25 22:06:50 +0800

    Update photo uploading logic for creating product variant

--- src/backend/controllers/productVariants/mutations/update_product_variant.ts
+++ src/backend/controllers/productVariants/mutations/update_product_variant.ts
@@ -1,20 +1,33 @@
 import { Context } from 'types/context'
 import { ProductVariant, UpdateProductVariantArgs } from 'types/productVariant'
+import { UploadImageArgs } from 'types/image'
+import { UploadImageType } from 'types/_enums/uploadImageType'
 import { AuditLogAction } from 'types/_enums/auditLogAction'
+import { handleUploadImage } from 'backend/_utils/handleImages/uploadImage'
+import { handleDeleteImage } from 'backend/_utils/handleImages/deleteImage'
 
 export default async (
   _root: undefined,
   args: UpdateProductVariantArgs,
   context: Context
 ): Promise<ProductVariant> => {
-  const { _id, name, price } = args
+  const { _id, _productId, image, imageUrl, name, price } = args
+
+  await handleDeleteImage(imageUrl)
+  const uploadImage: UploadImageArgs = {
+    imageType: UploadImageType.PRODUCT_VARIANT,
+    image: image,
+    productId: String(_productId),
+    productVariantName: name
+  }
+  const modifiedImageUrl = await handleUploadImage(uploadImage)
 
   const updateProductVariant: Partial<UpdateProductVariantArgs> = {
+    imageUrl: modifiedImageUrl,
     name: name,
     price: price,
     updatedAt: new Date()
   }
-
   const productVariant: any = await context.database.productVariants.findOneAndUpdate({ _id: _id }, updateProductVariant)
 
   await context.database.auditLogs.insertOne({
