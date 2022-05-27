428f2c34cde64b5cda63861d2d3088a4dcc767ee
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-27 23:29:56 +0800

    Create update_product_category mutation

---
+++ src/backend/controllers/productCategories/mutations/index.ts
@@ -0,0 +1,9 @@
+import create_product_category from './create_product_category'
+import update_product_category from './update_product_category'
+import delete_product_category from './delete_product_category'
+
+export default {
+  create_product_category,
+  update_product_category,
+  delete_product_category
+}
---
+++ src/backend/controllers/productCategories/mutations/update_product_category.ts
@@ -0,0 +1,26 @@
+import { Context } from 'types/context'
+import { ProductCategory, UpdateProductCategoryArgs } from 'types/ProductCategory'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: UpdateProductCategoryArgs,
+  context: Context
+): Promise<ProductCategory> => {
+  const { _id, name } = args
+
+  const updateProductCategory: Partial<UpdateProductCategoryArgs> = {
+    name: name,
+    updatedAt: new Date()
+  }
+  const productCategory: any = await context.database.productCategories.findOneAndUpdate({ _id: _id }, updateProductCategory)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.UPDATE_PRODUCT_CATEGORY,
+    productCategoryId: productCategory._id,
+    createdAt: new Date(),
+    createdBy: context.currentUserId
+  })
+
+  return productCategory
+}
