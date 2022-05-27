43d26bd554d22fe6e28733a8da8c33da34daddac
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-05-27 22:00:11 +0800

    Create update_faq mutation

---
+++ src/backend/controllers/faqs/mutations/update_faq.ts
@@ -0,0 +1,27 @@
+import { Context } from 'types/context'
+import { FAQ, UpdateFAQArgs } from 'types/faq'
+import { AuditLogAction } from 'types/_enums/auditLogAction'
+
+export default async (
+  _root: undefined,
+  args: UpdateFAQArgs,
+  context: Context
+): Promise<FAQ> => {
+  const { _id, question, answer } = args
+
+  const updateFAQ: Partial<UpdateFAQArgs> = {
+    question: question,
+    answer: answer,
+    updatedAt: new Date()
+  }
+  const faq: any = await context.database.faqs.findOneAndUpdate({ _id: _id }, updateFAQ)
+
+  await context.database.auditLogs.insertOne({
+    action: AuditLogAction.UPDATE_FAQ,
+    faqId: faq._id,
+    createdAt: new Date(),
+    createdBy: context.currentUserId
+  })
+
+  return faq
+}
