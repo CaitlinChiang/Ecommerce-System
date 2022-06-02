36eeac3a236c3f17095b84f29229b6e127c2a4f3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-02 15:15:28 +0800

    Create sign_in_user mutation

--- src/backend/controllers/users/mutations/index.ts
+++ src/backend/controllers/users/mutations/index.ts
@@ -1,8 +1,10 @@
+import sign_in_user from './sign_in_user'
 import create_user from './create_user'
 import update_user from './update_user'
 import delete_user from './delete_user'
 
 export default {
+  sign_in_user,
   create_user,
   update_user,
   delete_user
---
+++ src/backend/controllers/users/mutations/sign_in_user.ts
@@ -0,0 +1,30 @@
+import { Context } from 'types/context'
+import { User, SignInUserArgs } from 'types/user'
+import { AuthenticationError } from 'apollo-server-express'
+import bcrypt from 'bcrypt'
+import { generateJWT } from 'backend/_utils/jwt'
+
+export default async (
+  _root: undefined,
+  args: SignInUserArgs,
+  context: Context
+): Promise<User> => {
+  const { email, password } = args
+
+  const existingUser = await context.database.users.findOne({ email: email })
+  if (!existingUser) {
+    throw new AuthenticationError('Invalid email, please try again.')
+  }
+
+  const validatePassword = await bcrypt.compare(password, existingUser.password)
+  if (!validatePassword) {
+    throw new AuthenticationError('Incorrect password, please try again.')
+  }
+
+  const token = await generateJWT(existingUser._id)
+  
+  return {
+    ...existingUser,
+    token
+  }
+}
