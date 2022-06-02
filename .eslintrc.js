9a14cdeaa7c7cdad5c3df0b27de3c053fd9c65fc
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-02 21:48:35 +0800

    change settings of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,11 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/camelcase': ['off', { properties: 'never' }],
-    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-namespace': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
     '@typescript-eslint/quotes': ['error', 'single', { avoidEscape: true }],
-    camelcase: 'off',
+    'camelcase': 'off',
     'comma-dangle': 2,
     'no-extra-semi': 2,
     'no-irregular-whitespace': 2,
@@ -19,10 +21,11 @@ module.exports = {
     'no-multi-spaces': 2,
     'no-multiple-empty-lines': 1,
     'no-trailing-spaces': 2,
+    'no-control-regex': 0,
     'no-unexpected-multiline': 2,
     'no-unreachable': 'error',
     'object-curly-spacing': ['error', 'always'],
-    curly: 'error',
-    semi: ['error', 'never']
+    'quotes': 'off',
+    'semi': ['error', 'never']
   }
 }
