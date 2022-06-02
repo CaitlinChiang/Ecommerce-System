2ad56b89aa0570978155ad9c6a7d7c8ad90071b0
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-02 14:03:13 +0800

    Add an edit item quantity mutation

---
+++ src/backend/controllers/cart/mutations/edit_item_quantity.ts
@@ -0,0 +1,30 @@
+import { Context } from 'types/context'
+import { Cart, AddToCartArgs } from 'types/cart'
+
+export default async (
+  _root: undefined,
+  args: AddToCartArgs,
+  context: Context
+): Promise<Cart> => {
+  const { product, productVariant } = args
+
+  const cart: any = await context.database.carts.findOneAndUpdate(
+    { 
+      _userId: context.currentUserId,
+      products: { 
+        $elemMatch: { _id: product.productId, }
+      },
+      productVariants: { 
+        $elemMatch: { _id: productVariant.productVariantId, }
+      },
+    }, 
+    {
+      $set: {
+        'products.$.quantity': product.quantity,
+        'productVariants.$.quantity': productVariant.quantity
+      }
+    }
+  )
+  
+  return cart
+}
--- src/backend/controllers/cart/mutations/index.ts
+++ src/backend/controllers/cart/mutations/index.ts
@@ -1,7 +1,9 @@
 import add_to_cart from './add_to_cart'
+import edit_item_quantity from './edit_item_quantity'
 import remove_from_cart from './remove_from_cart'
 
 export default {
   add_to_cart,
+  edit_item_quantity,
   remove_from_cart
 }
