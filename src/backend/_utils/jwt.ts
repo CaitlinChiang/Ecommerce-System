e3bc864f132660267ae96926f66b9547c4f675ab
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-02 16:16:10 +0800

    Edit verifyJWT function

--- src/backend/_utils/jwt.ts
+++ src/backend/_utils/jwt.ts
@@ -1,27 +1,30 @@
 import { ObjectId } from 'mongodb'
 import jwt from 'jsonwebtoken'
-import { AuthenticationError } from 'apollo-server-express'
 
 const SECRET_KEY = process.env.JWT_SECRET
 
 const jwtSettings = {
-  expiresIn: '1h'
+  expiresIn: '10h'
 }
 
 export const generateJWT = (_id: ObjectId): string => {
   return jwt.sign({ _id }, SECRET_KEY, jwtSettings)
 }
 
-export const verifyJWT = (context) => {
-  const authHeader = context.req.headers.authorization
-  if (!authHeader) throw new Error('Authentication header must be provided.')
-
-  const token = authHeader.split('Bearer ')[1]
-  if (!token) throw new Error ("Authentication token must be 'Bearer [token].")
-
+export const verifyJWT = (
+  token: string
+): {
+  _id: ObjectId
+  iat: number
+  exp: number
+} => {
   try {
-    return jwt.verify(token, SECRET_KEY)
-  } catch (err) {
-    throw new AuthenticationError('Invalid / Expired Token.')
+    return jwt.verify(token, SECRET_KEY) as {
+      _id: ObjectId
+      iat: number
+      exp: number
+    }
+  } catch {
+    return
   }
 }
