728df0bdc8863e063a7d31e52e9338d0a88315b2
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-03 16:32:24 +0800

    Add prettier to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   rules: {
     '@typescript-eslint/camelcase': ['off', { properties: 'never' }],
