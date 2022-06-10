31b246493eae819c658b865e4b3db5259adddd30
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-10 21:18:54 +0800

    Set default color scheme

---
+++ src/frontend/themes/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+export default createTheme({
+  palette: {
+    primary: { main: '#A88238' },
+    secondary: { main: '#794618' },
+    error: red
+  }
+})
