fa2ab510fc0cc95898119fd96e4a2b86c28eaf40
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-11 00:38:51 +0800

    Create navigation bar with routing

--- src/frontend/layouts/navbar.tsx
+++ src/frontend/layouts/navbar.tsx
@@ -1,13 +1,75 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
 import React, { ReactElement } from 'react'
+import { useRouter } from 'next/router'
+import {
+  AppBar,
+  List,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography
+} from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import ReceiptIcon from '@mui/icons-material/Receipt'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+
+const shoppingMenu = [
+  { label: 'Home', route: '/home' },
+  { label: 'About', route: '/about' },
+  { label: 'Shop', route: '/shop' },
+  { label: 'Reviews', route: '/reviews' },
+  { label: 'FAQ', route: '/frequently-asked-questions' },
+  { label: 'Contact Us', route: '/contact-us' }
+]
+
+const trackingMenu = [
+  { icon: <ShoppingCartIcon />, route: '/cart' },
+  { icon: <ReceiptIcon />, route: '/my-orders' },
+  { icon: <AccountCircleIcon />, route: '/profile' }
+]
 
 const Navbar = (): ReactElement => {
+  const router = useRouter()
+
   return (
     <AppBar position={'static'}>
       <Toolbar>
         <Typography variant={'h6'} sx={{ flexGrow: 1 }}>
           {'Logo'}
         </Typography>
+        <List
+          sx={{
+            display: 'flex'
+          }}
+        >
+          {shoppingMenu.map((menuItem): ReactElement => {
+            return (
+              <ListItemButton
+                onClick={(): void => {
+                  router.push(menuItem.route)
+                }}
+              >
+                <ListItemText primary={menuItem.label.toUpperCase()} />
+              </ListItemButton>
+            )
+          })}
+        </List>
+        <List
+          sx={{
+            display: 'flex'
+          }}
+        >
+          {trackingMenu.map((menuItem): ReactElement => {
+            return (
+              <ListItemButton
+                onClick={(): void => {
+                  router.push(menuItem.route)
+                }}
+              >
+                {menuItem.icon}
+              </ListItemButton>
+            )
+          })}
+        </List>
       </Toolbar>
     </AppBar>
   )
