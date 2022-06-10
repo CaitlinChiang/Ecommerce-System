cfdfe08fa083a543a3c18926c12b8a1a409c415a
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-10 22:23:27 +0800

    Create layouts

---
+++ src/frontend/layouts/index.tsx
@@ -0,0 +1,63 @@
+import { NextPage } from 'next'
+import React, { ReactElement, FunctionComponent } from 'react'
+import theme from '../themes'
+import styled from '@emotion/styled'
+import Navbar from './Navbar'
+import Header from './Header'
+import classnames from 'classnames'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    margin: 'auto',
+    marginTop: theme.spacing(4),
+    minHeight: process.browser
+      ? window.innerHeight * 0.75 - Number(theme.mixins.toolbar.minHeight)
+      : '75vh'
+  },
+  narrow: {
+    maxWidth: theme.spacing(128)
+  },
+  wide: {}
+}
+
+const Div = styled('div')({
+  [theme.breakpoints.down('md')]: {
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    marginBottom: theme.spacing(8)
+  },
+  [theme.breakpoints.up('md')]: {
+    marginTop: theme.spacing(),
+    marginRight: theme.spacing(),
+    marginLeft: theme.spacing(),
+    marginBottom: theme.spacing(4)
+  }
+})
+
+export default (
+    Page: FunctionComponent,
+    { title, backRoute, wide }: { title: string; backRoute: string; wide?: boolean }
+  ) =>
+  (): FunctionComponent | NextPage | ReactElement => {
+    return (
+      <>
+        <Navbar />
+        <Header pageTitle={title} backRoute={backRoute} />
+        <div
+          className={
+            wide
+              ? classnames(styles.root, styles.wide)
+              : classnames(styles.root, styles.narrow)
+          }
+        >
+          <Div>
+            <Page />
+          </Div>
+        </div>
+      </>
+    )
+  }
