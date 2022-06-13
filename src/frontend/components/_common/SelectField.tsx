1a004023d250d2097eeee7932327c815e1b4ca74
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-13 14:54:34 +0800

    Add features for defaultValue and dynamic optionLabel and value

--- src/frontend/components/_common/SelectField.tsx
+++ src/frontend/components/_common/SelectField.tsx
@@ -2,23 +2,33 @@ import { ReactElement } from 'react'
 import { TextField, Autocomplete } from '@mui/material'
 
 const SelectField = ({
+  defaultValue,
   label,
-  setValue,
-  value,
+  optionLabel,
   options,
+  setValue,
+  targetProperty,
+  value,
   width
 }: {
+  defaultValue?: any
   label: string
   options: any
+  optionLabel?: string
   setValue: React.Dispatch<React.SetStateAction<string>>
+  targetProperty?: string
   value: string
   width?: number
 }): ReactElement => {
   return (
     <Autocomplete
-      getOptionLabel={(option): string => option}
-      onChange={(_e: any, newValue: string | null) => {
-        setValue(newValue)
+      defaultValue={defaultValue}
+      getOptionLabel={(option): string =>
+        optionLabel ? option[optionLabel] : option
+      }
+      onChange={(_e: any, newValue: any | null) => {
+        const val = targetProperty ? newValue[targetProperty] : newValue
+        setValue(val)
       }}
       options={options}
       renderInput={(params): ReactElement => <TextField {...params} label={label} />}
