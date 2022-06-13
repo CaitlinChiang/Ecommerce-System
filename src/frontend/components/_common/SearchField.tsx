981a2175ad96214fc576fcb23d7fb2dd28d3bf4e
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-13 17:39:32 +0800

    Create search field

---
+++ src/frontend/components/_common/SearchField.tsx
@@ -0,0 +1,88 @@
+import React, { ReactElement, KeyboardEvent } from 'react'
+import theme from '../../themes'
+import { Button, IconButton, InputAdornment, TextField } from '@mui/material'
+import CancelIcon from '@mui/icons-material/Cancel'
+import SearchIcon from '@mui/icons-material/Search'
+import { SearchTableQueryArgs } from '../../../types/actions/searchTableQuery'
+
+const SearchField = ({
+  helperText,
+  onBlur,
+  onKeyDown,
+  onSearch,
+  searchButtonDisabled,
+  searchText,
+  setSearchTableQuery
+}: {
+  helperText?: string
+  onBlur?: VoidFunction
+  onKeyDown?: (e: KeyboardEvent<HTMLDivElement>) => void
+  onSearch?: () => void
+  searchButtonDisabled?: boolean
+  searchText: string
+  setSearchTableQuery: React.Dispatch<React.SetStateAction<SearchTableQueryArgs>>
+}): ReactElement => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'start',
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2)
+      }}
+    >
+      <TextField
+        fullWidth
+        helperText={helperText}
+        onChange={(e): void => {
+          setSearchTableQuery({ searchText: e.target.value })
+        }}
+        onKeyDown={onKeyDown}
+        placeholder={'Search'}
+        size={'small'}
+        sx={{ marginRight: theme.spacing() }}
+        value={searchText}
+        variant={'outlined'}
+        InputProps={{
+          onBlur: onBlur ? onBlur : null,
+          onKeyDown: (e): void => {
+            if (onBlur && e.keyCode === 13) {
+              onBlur()
+            }
+          },
+          startAdornment: (
+            <InputAdornment position={'start'}>
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position={'end'}>
+              <IconButton
+                edge={'end'}
+                onClick={(): void => {
+                  setSearchTableQuery({ searchText: '' })
+                }}
+              >
+                <CancelIcon />
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
+      />
+      {onSearch && (
+        <Button
+          color={'primary'}
+          disabled={searchButtonDisabled}
+          onClick={onSearch}
+          variant={'contained'}
+        >
+          {'Search'}
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export default SearchField
