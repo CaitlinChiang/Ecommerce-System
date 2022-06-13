707feb12f6290a67b369d25cefc353b70350c651
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-13 15:07:13 +0800

    Create TextField

---
+++ src/frontend/components/_common/TextField.tsx
@@ -0,0 +1,40 @@
+import { ReactElement } from 'react'
+import { TextField } from '@mui/material'
+
+const Text = ({
+  defaultValue,
+  label,
+  maxRows,
+  multiline,
+  placeholder,
+  required,
+  setValue,
+  value
+}: {
+  defaultValue?: any
+  label: string
+  maxRows?: number
+  multiline?: boolean
+  placeholder?: string
+  required?: boolean
+  setValue: React.Dispatch<React.SetStateAction<string>>
+  value: string
+}): ReactElement => {
+  return (
+    <TextField
+      defaultValue={defaultValue}
+      id='outlined-required'
+      label={label}
+      maxRows={maxRows}
+      multiline={multiline}
+      onChange={(e): void => {
+        setValue(e.target.value)
+      }}
+      placeholder={placeholder}
+      value={value}
+      required={required}
+    />
+  )
+}
+
+export default Text
