829e1ffcfb710742bb77aebdb68bd9cbd0a97841
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-14 15:25:22 +0800

    Create returnMutationArgs

---
+++ src/backend/_utils/helpers/returnMutationArgs.ts
@@ -0,0 +1,18 @@
+import { MutateAction } from '../../../types/_enums/mutateAction'
+
+export const mutationArgs = (args: any, action: MutateAction): any => {
+  const modifiedArgs: any = {
+    args
+  }
+
+  switch (action) {
+    case MutateAction.CREATE:
+      modifiedArgs.createdAt = new Date()
+      break
+    case MutateAction.UPDATE:
+      modifiedArgs.updatedAt = new Date()
+      break
+  }
+
+  return modifiedArgs
+}
