366ee8b5c35e33d3dcea4b7a72c397d6e3a86c85
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-14 16:46:02 +0800

    Component types are all explicit to the module for readability

--- src/frontend/components/_common/DropdownsComponent.tsx
+++ src/frontend/components/_common/DropdownsComponent.tsx
@@ -12,7 +12,7 @@ import {
 } from '@mui/material'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
-import { DropdownRow } from '../../../types/_components/dropdownRow'
+import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
 
 const DropdownsComponent = ({
   open,
@@ -20,40 +20,49 @@ const DropdownsComponent = ({
   setOpen
 }: {
   open: boolean
-  rows: DropdownRow[]
+  rows: { title: string; content: ReactJSXElement }[]
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }): ReactElement => {
   return (
     <TableContainer component={Paper}>
       <Table aria-label='collapsible table'>
         <TableBody>
-          {rows.map((row: DropdownRow): ReactElement => {
-            return (
-              <>
-                <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                  <TableCell component='th' scope='row'>
-                    {row.title}
-                  </TableCell>
-                  <TableCell align={'right'}>
-                    <IconButton
-                      aria-label='expand row'
-                      onClick={() => setOpen(!open)}
-                      size='small'
+          {rows.map(
+            (row: { title: string; content: ReactJSXElement }): ReactElement => {
+              return (
+                <>
+                  <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                    <TableCell component='th' scope='row'>
+                      {row.title}
+                    </TableCell>
+                    <TableCell align={'right'}>
+                      <IconButton
+                        aria-label='expand row'
+                        onClick={() => setOpen(!open)}
+                        size='small'
+                      >
+                        {open ? (
+                          <KeyboardArrowDownIcon />
+                        ) : (
+                          <KeyboardArrowRightIcon />
+                        )}
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell
+                      colSpan={6}
+                      style={{ paddingBottom: 0, paddingTop: 0 }}
                     >
-                      {open ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell colSpan={6} style={{ paddingBottom: 0, paddingTop: 0 }}>
-                    <Collapse in={open} timeout='auto' unmountOnExit>
-                      <Box sx={{ margin: 1 }}>{row.content}</Box>
-                    </Collapse>
-                  </TableCell>
-                </TableRow>
-              </>
-            )
-          })}
+                      <Collapse in={open} timeout='auto' unmountOnExit>
+                        <Box sx={{ margin: 1 }}>{row.content}</Box>
+                      </Collapse>
+                    </TableCell>
+                  </TableRow>
+                </>
+              )
+            }
+          )}
         </TableBody>
       </Table>
     </TableContainer>
--- src/types/_components/dropdownRow.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
-
-export interface DropdownRow {
-  title: string
-  content: ReactJSXElement
-}
