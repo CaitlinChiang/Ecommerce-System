0dd51c5b52ee101ca68c577905ca82f54162f7b7
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-15 23:02:32 +0800

    Fix 404 page

--- src/frontend/pages/404.tsx
+++ src/frontend/pages/404.tsx
@@ -1,12 +1,10 @@
 import { NextPage } from 'next'
 import React, { ReactElement } from 'react'
-import Navbar from '../layouts/consumer/Navbar'
 import Header from '../layouts/consumer/Header'
 
 const NotFoundPage: NextPage = (): ReactElement => {
   return (
     <>
-      <Navbar />
       <Header pageTitle='Page Not Found' backRoute='/' />
     </>
   )
