3c38f9b2631b050fc4bda778ae98ad0034957810
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-15 23:01:47 +0800

    Fix requirement for websitetext query

--- src/frontend/components/websiteTexts/Showcase/query.ts
+++ src/frontend/components/websiteTexts/Showcase/query.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export default gql`
-  query ($_id: ID, type: String) {
+  query ($_id: ID, $type: String!) {
     get_website_text(_id: $_id, type: $type) {
       _id
     }
