148f7cd0a171e0596f098ac60f1d7b90cbccab23
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-15 17:20:46 +0800

    Add layout to homepage

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,15 +1,14 @@
 import { NextPage } from 'next'
 import React, { ReactElement } from 'react'
-import Navbar from '../layouts/consumer/Navbar'
-import Header from '../layouts/consumer/Header'
+import Header from '../layouts/admin/Header'
+import layout from '../layouts'
 
 const Home: NextPage = (): ReactElement => {
   return (
     <>
-      <Navbar />
-      <Header pageTitle={'Test Page Header'} />
+      <Header pageTitle={'Welcome Back, Caitlin!'} />
     </>
   )
 }
 
-export default Home
+export default layout(Home, { title: 'Home', backRoute: '/' })
