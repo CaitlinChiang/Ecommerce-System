ac015c33ab8e56f057a5067644614b3c43353ba0
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-17 18:46:32 +0800

    Create currentDateTime module function

---
+++ src/backend/_utils/helpers/returnCurrentDateTime.ts
@@ -0,0 +1,50 @@
+export const currentDateTime = () => {
+  const date = new Date()
+  const localDate = date.toLocaleString('en-US', { timeZone: 'Asia/Manila' })
+
+  const formattedDate = formatCurrentDate(localDate)
+  const formattedTime = formatCurrentTime(localDate)
+
+  return new Date(formattedDate + formattedTime)
+}
+
+const formatCurrentDate = (localDate: string) => {
+  const currentDate = localDate.split(', ')[0]
+  const splitDate = currentDate.split('/')
+
+  const month = addZeroIfNeeded(splitDate[0])
+  const day = addZeroIfNeeded(splitDate[1])
+  const year = addZeroIfNeeded(splitDate[2])
+
+  return year + '-' + month + '-' + day
+}
+
+const formatCurrentTime = (localDate: string) => {
+  const currentTime = localDate.split(', ')[1].split(' ')[0]
+  const currentTimeOfDay = localDate.split(', ')[1].split(' ')[1]
+  const splitTime = currentTime.split(':')
+
+  const hour = transformTo24HourTime(splitTime[0], currentTimeOfDay)
+  const minute = addZeroIfNeeded(splitTime[1])
+  const second = addZeroIfNeeded(splitTime[2])
+
+  const time = hour + ':' + minute + ':' + second
+
+  return 'T' + time + '.000+00:00'
+}
+
+const transformTo24HourTime = (hour: string, currentTimeOfDay: string) => {
+  switch (currentTimeOfDay) {
+    case 'AM':
+      return addZeroIfNeeded(hour)
+    case 'PM':
+      return String(12 + Number(hour))
+  }
+}
+
+const addZeroIfNeeded = (number: string) => {
+  if (String(number).length === 1) {
+    return '0' + number
+  }
+  return number
+}
