bba5c2656d2aed08bea99cf3ecd37c4ce7014417
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-18 00:57:41 +0800

    Add auditlogs table to pages

---
+++ src/frontend/pages/admin/audit-logs/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+import layout from '../../../layouts'
+import AuditLogsTable from '../../../components/auditLogs/Showcase/table'
+
+const Page = (): ReactElement => {
+  return (
+    <>
+      <AuditLogsTable />
+    </>
+  )
+}
+
+export default layout(Page, { title: 'Audit Logs', backRoute: '/' })
