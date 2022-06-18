52c9a2b0e67386a35528a9c0d051d5c1379fbdd0
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-18 14:23:10 +0800

    Create query for reviews table

--- src/frontend/components/reviews/Showcase/query.ts
+++ src/frontend/components/reviews/Showcase/query.ts
@@ -2,8 +2,12 @@ import { gql } from '@apollo/client'
 
 export default gql`
   query ($featured: Boolean, $paginateData: PaginateDataInput) {
-    get_reviews(featured: $featured) {
+    get_reviews(featured: $featured, paginateData: $paginateData) {
       _id
+      createdAt
+      username
+      content
+      featured
     }
 
     get_reviews_count(featured: $featured, paginateData: $paginateData)
