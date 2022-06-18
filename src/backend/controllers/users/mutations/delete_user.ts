d1f03b0099ffbda4b45109b32922cac6e4b2ea15
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-18 18:01:46 +0800

    Change delete function to update function to delete_user

--- src/backend/controllers/users/mutations/delete_user.ts
+++ src/backend/controllers/users/mutations/delete_user.ts
@@ -1,8 +1,10 @@
 import { ObjectId } from 'mongodb'
 import { Context } from '../../../../types/setup/context'
 import { User, DeleteUserArgs } from '../../../../types/user'
+import { MutateAction } from '../../../_enums/mutateAction'
 import { AuditLogAction } from '../../../_enums/auditLogAction'
 import { authenticateUser } from '../../../_utils/authenticateUser'
+import { mutationArgs } from '../../../_utils/helpers/returnMutationArgs'
 import { auditArgs } from '../../../_utils/helpers/returnAuditArgs'
 
 export default async (
@@ -12,9 +14,10 @@ export default async (
 ): Promise<User> => {
   authenticateUser({ admin: true }, context)
 
-  const user: any = await context.database.users.findOneAndDelete({
-    _id: new ObjectId(args._id)
-  })
+  const user: any = await context.database.users.findOneAndUpdate(
+    { _id: new ObjectId(args._id) },
+    { $set: mutationArgs(args, MutateAction.DELETE) }
+  )
 
   await context.database.auditLogs.insertOne({
     action: AuditLogAction.DELETE_USER,
