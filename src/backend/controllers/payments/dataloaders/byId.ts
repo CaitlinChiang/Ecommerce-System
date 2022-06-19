c94482588d66d4ddb964f6ca79ec36eb03654022
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-19 18:38:00 +0800

    Create payment dataloader

---
+++ src/backend/controllers/payments/dataloaders/byId.ts
@@ -0,0 +1,15 @@
+import { ObjectId } from 'mongodb'
+import { Database } from '../../../../types/setup/database'
+import { Payment } from '../../../../types/payment'
+
+export default async (db: Database, ids: ObjectId[]): Promise<Payment[]> => {
+  const payments: Payment[] = await db.payments.find({ _id: { $in: ids } }).toArray()
+
+  const paymentsById: { [id: string]: Payment } = {}
+
+  payments.forEach((payment: Payment): void => {
+    paymentsById[String(payment._id)] = payment
+  })
+
+  return ids.map((id: ObjectId): Payment => paymentsById[String(id)])
+}
