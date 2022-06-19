2a6a86616d261f22fb0b8276dcaa8a4b4a17c098
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-19 18:00:33 +0800

    Refactor

--- src/backend/controllers/users/index.ts
+++ src/backend/controllers/users/index.ts
@@ -1,9 +1,9 @@
+import { Database } from '../../../types/setup/database'
 import dataloaders, { UserDataloaders } from './dataloaders'
 import * as typeDefs from './typeDefs.graphql'
 import resolvers from './resolvers'
 import mutations from './mutations'
 import queries from './queries'
-import { Database } from '../../../types/setup/database'
 
 export default {
   dataloaders: (db: Database): UserDataloaders => dataloaders(db),
