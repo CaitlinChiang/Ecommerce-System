783c7a1025782778ad71822cbb404d255d92b069
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-19 23:30:45 +0800

    Export byIds dataloaders of products and productVariants

--- src/backend/controllers/products/dataloaders/index.ts
+++ src/backend/controllers/products/dataloaders/index.ts
@@ -3,11 +3,14 @@ import { ObjectId } from 'mongodb'
 import { Database } from '../../../../types/setup/database'
 import { Product } from '../../../../types/product'
 import byId from './byId'
+import byIds from './byIds'
 
 export interface ProductDataloaders {
   byId: Dataloader<ObjectId, Product, ObjectId[]>
+  byIds: Dataloader<ObjectId[], Product[], ObjectId[]>
 }
 
 export default (db: Database): ProductDataloaders => ({
-  byId: new Dataloader((ids: ObjectId[]): Promise<Product[]> => byId(db, ids))
+  byId: new Dataloader((ids: ObjectId[]): Promise<Product[]> => byId(db, ids)),
+  byIds: new Dataloader((ids: ObjectId[][]): Promise<Product[][]> => byIds(db, ids))
 })
