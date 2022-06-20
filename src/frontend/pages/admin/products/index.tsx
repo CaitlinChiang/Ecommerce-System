22b3e048af1e0d3067da3c20542cbe693d275b6e
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-21 03:10:54 +0800

    Add create button and route to page

--- src/frontend/pages/admin/products/index.tsx
+++ src/frontend/pages/admin/products/index.tsx
@@ -1,10 +1,23 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
+import { useRouter } from 'next/router'
+import { Button } from '@mui/material'
 import layout from '../../../layouts'
 import ProductsTable from '../../../components/products/Showcase/table'
 
 const Page = (): ReactElement => {
+  const router = useRouter()
+
   return (
     <>
+      <Button
+        color={'primary'}
+        fullWidth
+        onClick={() => router.push('products/create')}
+        type='submit'
+        variant={'contained'}
+      >
+        {'Create Product'}
+      </Button>
       <ProductsTable />
     </>
   )
