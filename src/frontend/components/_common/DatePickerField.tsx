0400484ce96963a4e447a424b90bb1a751674622
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-20 22:37:41 +0800

    Refactor DatePickerField

--- src/frontend/components/_common/DatePickerField.tsx
+++ src/frontend/components/_common/DatePickerField.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from 'react'
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker'
-import { TextField } from '@mui/material'
+import theme from '../../themes'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import { DatePicker } from '@mui/lab'
+import { Box, TextField } from '@mui/material'
 import { DateRange } from '../../../types/common/dateRange'
 import { formatProperCapitalization } from '../../_utils/formatProperCapitalization'
 
@@ -16,16 +19,20 @@ const DatePickerField = ({
   targetProperty: string
 }): ReactElement => {
   return (
-    <DesktopDatePicker
-      disabled={disabled}
-      inputFormat={'MM/dd/yyyy'}
-      label={formatProperCapitalization(targetProperty)}
-      onChange={(newValue: Date | null) => {
-        setDateRangeArgs({ ...dateRangeArgs, [targetProperty]: newValue })
-      }}
-      renderInput={(params) => <TextField {...params} />}
-      value={dateRangeArgs[targetProperty]}
-    />
+    <Box sx={{ padding: theme.spacing(2) }}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DatePicker
+          disabled={disabled}
+          inputFormat={'MM-dd-yyyy'}
+          label={formatProperCapitalization(targetProperty)}
+          onChange={(newValue: Date | null) => {
+            setDateRangeArgs({ ...dateRangeArgs, [targetProperty]: newValue })
+          }}
+          renderInput={(params) => <TextField {...params} />}
+          value={dateRangeArgs[targetProperty]}
+        />
+      </LocalizationProvider>
+    </Box>
   )
 }
 
