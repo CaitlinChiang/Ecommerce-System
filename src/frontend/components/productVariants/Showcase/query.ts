5302800691d6fc8dad513da787b7a197cfae60d8
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-21 03:17:02 +0800

    Add fields to productVariants query

--- src/frontend/components/productVariants/Showcase/query.ts
+++ src/frontend/components/productVariants/Showcase/query.ts
@@ -1,9 +1,18 @@
 import { gql } from '@apollo/client'
 
 export const querySingular = gql`
-  query ($_id: ID) {
+  query ($_id: ID!) {
     get_product_variant(_id: $_id) {
       _id
+      description
+      expirationDate
+      imageUrl
+      name
+      price
+      showPublic
+      stockQuantity
+      createdAt
+      updatedAt
     }
   }
 `
@@ -11,21 +20,29 @@ export const querySingular = gql`
 export const queryMultiple = gql`
   query (
     $_productId: ID
+    $dateRange: DateRangeInput
     $paginateData: PaginateDataInput
     $showPublic: Boolean
     $stockQuantity: StockQuantityInput
   ) {
     get_product_variants(
       _productId: $_productId
+      dateRange: $dateRange
       paginateData: $paginateData
       showPublic: $showPublic
       stockQuantity: $stockQuantity
     ) {
       _id
+      name
+      expirationDate
+      name
+      price
+      stockQuantity
     }
 
     get_product_variants_count(
       _productId: $_productId
+      dateRange: $dateRange
       paginateData: $paginateData
       showPublic: $showPublic
       stockQuantity: $stockQuantity
