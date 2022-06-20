4cde67f4e5c01828fce55622a240829bf22aa266
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-21 02:17:03 +0800

    Data renders upon load

--- src/frontend/components/products/Update/index.tsx
+++ src/frontend/components/products/Update/index.tsx
@@ -1,13 +1,18 @@
-import { ReactElement, useState } from 'react'
+import { ReactElement, useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/client'
-import query from '../Showcase/query'
-import { Checkbox } from '@mui/material'
+import { querySingular } from '../Showcase/query'
+import { Typography } from '@mui/material'
 import { Product } from '../../../../types/product'
 import ProductCategoriesSelect from '../../../components/productCategories/Showcase/select'
 import Text from '../../_common/TextField'
-import DatePickerField from 'frontend/components/_common/DatePickerField'
+import DatePickerField from '../../_common/DatePickerField'
+import CheckboxField from '../../_common/CheckboxField'
+import NumberField from '../../_common/NumberField'
 
 const UpdateProduct = (): ReactElement => {
+  const router = useRouter()
+
   const [args, setArgs] = useState<any>({
     categoryId: null,
     description: null,
@@ -19,42 +24,52 @@ const UpdateProduct = (): ReactElement => {
     stockQuantity: null
   })
 
-  const { data } = useQuery(query, {
-    variables: args,
-    onCompleted: (product): void => {
-      setArgs({
-        categoryId: product?.categoryId,
-        description: product?.description,
-        expirationDate: product?.expirationDate,
-        featured: product?.featured,
-        name: product?.name,
-        price: product?.price,
-        showPublic: product?.showPublic,
-        stockQuantity: product?.stockQuantity
-      })
-    }
+  const { data } = useQuery(querySingular, {
+    variables: { _id: '62b036fe3fcf87061111d52c' }
   })
 
   const product: Product = data?.get_product || {}
 
+  useEffect(() => {
+    setArgs({
+      categoryId: product?.categoryId,
+      description: product?.description,
+      expirationDate: product?.expirationDate,
+      featured: product?.featured,
+      name: product?.name,
+      price: product?.price,
+      showPublic: product?.showPublic,
+      stockQuantity: product?.stockQuantity
+    })
+  }, [data])
+
   return (
     <>
-      <ProductCategoriesSelect args={args} setArgs={setArgs} />
-      <Text
-        args={args}
-        label={'Description'}
-        setArgs={setArgs}
-        targetProperty={'description'}
-        width={800}
-      />
+      <Typography>{`Created At: ${product?.createdAt}`}</Typography>
+      {product?.updatedAt && (
+        <Typography>{`Last Updated At: ${product?.updatedAt}`}</Typography>
+      )}
+      <ProductCategoriesSelect args={args} required={true} setArgs={setArgs} />
+      <Text args={args} setArgs={setArgs} targetProperty={'description'} />
       <DatePickerField
         args={args}
         setArgs={setArgs}
         targetProperty={'expirationDate'}
       />
-      <Checkbox
-        checked={args?.featured}
-        onChange={() => setArgs({ ...args, featured: !args?.featured })}
+      <CheckboxField args={args} setArgs={setArgs} targetProperty={'featured'} />
+      <Text args={args} required={true} setArgs={setArgs} targetProperty={'name'} />
+      <NumberField
+        args={args}
+        required={true}
+        setArgs={setArgs}
+        targetProperty={'price'}
+      />
+      <CheckboxField args={args} setArgs={setArgs} targetProperty={'showPublic'} />
+      <NumberField
+        args={args}
+        required={true}
+        setArgs={setArgs}
+        targetProperty={'stockQuantity'}
       />
     </>
   )
