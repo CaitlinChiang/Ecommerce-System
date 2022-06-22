2ec20a39f1aee3d66e0374b331124bb66ca39eec
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-22 19:23:06 +0800

    Fix query for get_website_text

--- src/frontend/components/websiteTexts/Showcase/query.ts
+++ src/frontend/components/websiteTexts/Showcase/query.ts
@@ -1,9 +1,10 @@
 import { gql } from '@apollo/client'
 
 export default gql`
-  query ($_id: ID, $type: String!) {
-    get_website_text(_id: $_id, type: $type) {
+  query ($type: String!) {
+    get_website_text(type: $type) {
       _id
+      content
     }
   }
 `
