bd3a892e17b1e6173e901e91b83fb4d89de1c8a7
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-23 02:22:32 +0800

    Create tableFilters for product categories

---
+++ src/frontend/components/productCategories/Showcase/tableFilters.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react'
+import SelectField from '../../_common/SelectField'
+
+const ProductCategoriesTableFilters = ({
+  args,
+  setArgs
+}: {
+  args: any
+  setArgs: React.Dispatch<React.SetStateAction<any>>
+}): ReactElement => {
+  return (
+    <SelectField
+      args={args}
+      label={'Show Public Status'}
+      optionLabelProp={'label'}
+      options={[
+        { label: 'Public Categories', showPublic: true },
+        { label: 'Private Categories', showPublic: false }
+      ]}
+      setArgs={setArgs}
+      targetProp={'showPublic'}
+    />
+  )
+}
+
+export default ProductCategoriesTableFilters
