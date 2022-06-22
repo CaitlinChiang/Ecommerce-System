59b2be41da24b04f15096913650c81fdd32a8b18
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-23 02:20:02 +0800

    Add fields to product category field

--- src/frontend/components/productCategories/Showcase/query.ts
+++ src/frontend/components/productCategories/Showcase/query.ts
@@ -4,19 +4,26 @@ export const querySingular = gql`
   query ($_id: ID!) {
     get_product_category(_id: $_id) {
       _id
+      name
+      showPublic
+      createdAt
+      updatedAt
     }
   }
 `
 
 export const queryMultiple = gql`
-  query ($paginateData: PaginateDataInput, showPublic: Boolean) {
+  query ($paginateData: PaginateDataInput, $showPublic: Boolean) {
     get_product_categories(paginateData: $paginateData, showPublic: $showPublic) {
       _id
       name
+      showPublic
       createdAt
-      updatedAt
     }
 
-    get_product_categories_count(paginateData: $paginateData, showPublic: $showPublic)
+    get_product_categories_count(
+      paginateData: $paginateData
+      showPublic: $showPublic
+    )
   }
 `
