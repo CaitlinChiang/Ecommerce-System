f4481526af6dae83a52c0ac3dbcf6b4b551c64fc
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-23 23:30:43 +0800

    Add needed fields to cities get queryies

--- src/frontend/components/cities/Showcase/query.ts
+++ src/frontend/components/cities/Showcase/query.ts
@@ -4,6 +4,10 @@ export const querySingular = gql`
   query ($_id: ID!) {
     get_city(_id: $_id) {
       _id
+      name
+      shippingFee
+      createdAt
+      updatedAt
     }
   }
 `
@@ -12,6 +16,8 @@ export const queryMultiple = gql`
   query ($paginateData: PaginateDataInput) {
     get_cities(paginateData: $paginateData) {
       _id
+      name
+      shippingFee
     }
 
     get_cities_count(paginateData: $paginateData)
