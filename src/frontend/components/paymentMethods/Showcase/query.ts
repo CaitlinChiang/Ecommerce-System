11c4e6ee1336d6d012a56d77ad05bebd5d9933d4
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-23 19:15:15 +0800

    Add query to payment methods

--- src/frontend/components/paymentMethods/Showcase/query.ts
+++ src/frontend/components/paymentMethods/Showcase/query.ts
@@ -4,6 +4,10 @@ export const querySingular = gql`
   query ($_id: ID!) {
     get_payment_method(_id: $_id) {
       _id
+      name
+      details
+      createdAt
+      updatedAt
     }
   }
 `
@@ -12,6 +16,8 @@ export const queryMultiple = gql`
   query (paginateData: PaginateDataInput) {
     get_payment_methods(paginateData: $paginateData) {
       _id
+      name
+      details
     }
 
     get_payment_methods_count(paginateData: $paginateData)
