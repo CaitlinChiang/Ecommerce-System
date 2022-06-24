167a4809a5ea21e367b0c0d7989e502957d8aa7c
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-25 00:58:56 +0800

    Create byId dataloader for productVariant

--- src/backend/controllers/productVariants/dataloaders/index.ts
+++ src/backend/controllers/productVariants/dataloaders/index.ts
@@ -2,17 +2,17 @@ import Dataloader from 'dataloader'
 import { ObjectId } from 'mongodb'
 import { Database } from '../../../../types/setup/database'
 import { ProductVariant } from '../../../../types/productVariant'
-import byIds from './byIds'
+import byId from './byId'
 import byProductId from './byProductId'
 
 export interface ProductVariantDataloaders {
-  byIds: Dataloader<ObjectId[], ProductVariant[], ObjectId[]>
+  byId: Dataloader<ObjectId, ProductVariant, ObjectId[]>
   byProductId: Dataloader<ObjectId, ProductVariant[], ObjectId[]>
 }
 
 export default (db: Database): ProductVariantDataloaders => ({
-  byIds: new Dataloader(
-    (ids: ObjectId[][]): Promise<ProductVariant[][]> => byIds(db, ids)
+  byId: new Dataloader(
+    (ids: ObjectId[]): Promise<ProductVariant[]> => byId(db, ids)
   ),
   byProductId: new Dataloader(
     (ids: ObjectId[]): Promise<ProductVariant[][]> => byProductId(db, ids)
