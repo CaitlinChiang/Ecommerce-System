65e1aec5a8dc5b1e9806bd9fa89304e16376ca69
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-25 00:57:34 +0800

    Apply paymentMethod dataloader

--- src/backend/controllers/payments/resolvers.ts
+++ src/backend/controllers/payments/resolvers.ts
@@ -1,8 +1,20 @@
+import { Context } from '../../../types/setup/context'
 import { Payment } from '../../../types/payment'
+import { PaymentMethod } from '../../../types/paymentMethod'
 import { formatDateTime } from '../../_utils/helpers/dateFormatters/formatDateTime'
 
 export default {
   Payment: {
+    paymentMethod: async (
+      payment: Payment,
+      args: undefined,
+      context: Context
+    ): Promise<PaymentMethod> => {
+      const paymentMethod: PaymentMethod =
+        await context.dataloaders.paymentMethods.byId.load(payment?.paymentMethodId)
+      return paymentMethod
+    },
+
     createdAt: async (payment: Payment): Promise<string> => {
       return formatDateTime(payment?.createdAt) || '-'
     },
