7da1c61c5afcc38240d590fcf63b62202b2d98d4
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-24 23:46:15 +0800

    Refactor + Add new logic for correctArgs function

--- src/backend/_utils/helpers/correctArgs.ts
+++ src/backend/_utils/helpers/correctArgs.ts
@@ -1,17 +1,40 @@
 import { ObjectId } from 'mongodb'
 
 export const correctArgs = (modifiedArgs: any, mutation: boolean): any => {
-  Object.keys(modifiedArgs).forEach((key) => {
-    if (modifiedArgs[key] === null) {
-      delete modifiedArgs[key]
+  Object.keys(modifiedArgs).forEach((key: any) => {
+    // LOGIC IF THE KEY IS AN OBJECT
+    if (typeof key === 'object' && !Array.isArray(key) && key != null) {
+      Object.keys(key).forEach((nestedKey: string) => {
+        modifyArgs(nestedKey, key[nestedKey], mutation)
+      })
     }
 
-    if (modifiedArgs[key] != null && String(key).includes('Id')) {
-      modifiedArgs[key] = new ObjectId(modifiedArgs[key])
+    // LOGIC IF THE KEY IS AN ARRAY
+    if (Array.isArray(key) && key != null) {
+      key.forEach((obj: any) => {
+        Object.keys(obj).forEach((nestedKey: string) => {
+          modifyArgs(nestedKey, obj[nestedKey], mutation)
+        })
+      })
     }
 
-    if (mutation && modifiedArgs[key] != null && String(key).includes('Date')) {
-      modifiedArgs[key] = new Date(modifiedArgs[key])
+    // LOGIC IF THE KEY IS A STRING
+    if (typeof key !== 'object') {
+      modifyArgs(key, modifiedArgs, mutation)
     }
   })
+}
+
+const modifyArgs = (key: string, modifiedArgs: any, mutation: boolean): any => {
+  if (modifiedArgs[key] === null) {
+    delete modifiedArgs[key]
+  }
+
+  if (key.includes('Id') && modifiedArgs[key] != null) {
+    modifiedArgs[key] = new ObjectId(modifiedArgs[key])
+  }
+
+  if (key.includes('Date') && mutation && modifiedArgs[key] != null) {
+    modifiedArgs[key] = new Date(modifiedArgs[key])
+  }
 }
