12647cc01cdee1ec85136c6500a4eedf02a19cba
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-24 16:56:17 +0800

    Transfer formatToPercentage function to frontend

---
+++ src/frontend/_utils/formatToPercentage.ts
@@ -0,0 +1,7 @@
+export const formatToPercentage = (discount: number): string => {
+  if (!discount) return
+
+  const discountWholeNumber = String(discount * 100)
+
+  return discountWholeNumber + '%'
+}
