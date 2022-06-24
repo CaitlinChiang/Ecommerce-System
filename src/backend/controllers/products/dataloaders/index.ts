fb0448b83d81c470aea20ea11e8a30324a54132c
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-06-25 00:58:22 +0800

    Delete byIds dataloaders

--- src/backend/controllers/products/dataloaders/index.ts
+++ src/backend/controllers/products/dataloaders/index.ts
@@ -3,14 +3,11 @@ import { ObjectId } from 'mongodb'
 import { Database } from '../../../../types/setup/database'
 import { Product } from '../../../../types/product'
 import byId from './byId'
-import byIds from './byIds'
 
 export interface ProductDataloaders {
   byId: Dataloader<ObjectId, Product, ObjectId[]>
-  byIds: Dataloader<ObjectId[], Product[], ObjectId[]>
 }
 
 export default (db: Database): ProductDataloaders => ({
-  byId: new Dataloader((ids: ObjectId[]): Promise<Product[]> => byId(db, ids)),
-  byIds: new Dataloader((ids: ObjectId[][]): Promise<Product[][]> => byIds(db, ids))
+  byId: new Dataloader((ids: ObjectId[]): Promise<Product[]> => byId(db, ids))
 })
