7e1985bd670264cfcba755c1ece63c21a01d775d
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-01 19:00:20 +0800

    Create CreateFAQ component

---
+++ src/frontend/components/faqs/Create/index.tsx
@@ -0,0 +1,44 @@
+import { ReactElement, useState } from 'react'
+import { useMutation } from '@apollo/client'
+import mutation from './mutation'
+import { Button } from '@mui/material'
+import { RefetchDataArgs } from '../../../../types/actions/refetchData'
+import Text from '../../_common/TextField'
+import { refetchData } from '../../../_utils/refetchData'
+import { clearFields } from '../../../_utils/clearFields'
+
+const CreateFAQ = ({
+  refetchArgs
+}: {
+  refetchArgs: RefetchDataArgs
+}): ReactElement => {
+  const [args, setArgs] = useState<any>({
+    answer: null,
+    question: null
+  })
+
+  const [createMutation, createMutationState] = useMutation(mutation, {
+    variables: args,
+    onCompleted: () => {
+      console.log('FAQ successfully created!')
+      refetchData(refetchArgs)
+      setArgs(clearFields(args))
+    },
+    onError: (error) => console.log(error)
+  })
+
+  return (
+    <>
+      <Text args={args} required={true} setArgs={setArgs} targetProp={'question'} />
+      <Text args={args} required={true} setArgs={setArgs} targetProp={'answer'} />
+      <Button
+        onClick={() => createMutation()}
+        disabled={createMutationState.loading}
+      >
+        {'Create'}
+      </Button>
+    </>
+  )
+}
+
+export default CreateFAQ
