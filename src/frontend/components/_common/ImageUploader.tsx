ff61d54b9f9b21c4319f5c592c2576586f39cf7f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-02 03:10:28 +0800

    Create ImageUploader common component

---
+++ src/frontend/components/_common/ImageUploader.tsx
@@ -0,0 +1,56 @@
+import { ReactElement, useState, useEffect } from 'react'
+import { Box, Button } from '@mui/material'
+
+const ImageUploader = ({
+  alt,
+  args,
+  setArgs,
+  targetProp
+}: {
+  alt: string
+  args: any
+  setArgs: React.Dispatch<React.SetStateAction<any>>
+  targetProp: string
+}): ReactElement => {
+  const [imageUrl, setImageUrl] = useState<string>('')
+
+  useEffect(() => {
+    const existingUrl = targetProp + 'Url'
+
+    if (args?.[existingUrl]) {
+      setImageUrl(args?.[existingUrl])
+    }
+  }, [args])
+
+  const uploadImage = async (event: any) => {
+    const image = event.target.files[0]
+    setArgs({ ...args, [targetProp]: image })
+
+    const imageUrl = URL.createObjectURL(image)
+    setImageUrl(imageUrl)
+  }
+
+  return (
+    <>
+      <Button variant={'contained'}>
+        <input type='file' onChange={(event) => uploadImage(event)} />
+        {'Upload Product Photo'}
+      </Button>
+      {imageUrl && (
+        <Box
+          component='img'
+          sx={{
+            height: 233,
+            width: 350,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 }
+          }}
+          alt={alt}
+          src={imageUrl}
+        />
+      )}
+    </>
+  )
+}
+
+export default ImageUploader
