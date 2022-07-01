10802ac2b97754e6030c8cd4d9c1f4314e5e357c
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-02 00:33:10 +0800

    Require images in create_product

--- src/frontend/components/products/Create/mutation.ts
+++ src/frontend/components/products/Create/mutation.ts
@@ -7,7 +7,7 @@ export default gql`
     $discount: Float
     $expirationDate: String
     $featured: Boolean!
-    $image: Upload
+    $image: String!
     $name: String!
     $price: Float!
     $showPublic: Boolean!
