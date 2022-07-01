4eb3709781a6463fb4d95433399587c4809d84db
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-01 19:58:36 +0800

    Add fields to faq query

--- src/frontend/components/faqs/Showcase/query.ts
+++ src/frontend/components/faqs/Showcase/query.ts
@@ -4,6 +4,10 @@ export const querySingular = gql`
   query ($_id: ID!) {
     get_faq(_id: $_id) {
       _id
+      answer
+      question
+      createdAt
+      updatedAt
     }
   }
 `
@@ -12,6 +16,8 @@ export const queryMultiple = gql`
   query ($paginateData: PaginateDataInput) {
     get_faqs(paginateData: $paginateData) {
       _id
+      answer
+      question
     }
 
     get_faqs_count(paginateData: $paginateData)
