30b11e5f8cdaad378dbab358af873569a2870499
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-07 04:49:36 +0800

    Create a returnCurrentUser function for main index file

---
+++ src/backend/_utils/returnCurrentUser.ts
@@ -0,0 +1,21 @@
+import { ObjectId } from 'mongodb'
+import { User } from '../../types/user'
+import { generateJWT, verifyJWT } from './jwt'
+
+export default async (headers, database): Promise<User | null> => {
+  if (!headers.accesstoken) return null
+
+  const decoded = await verifyJWT(headers.accesstoken)
+
+  const user: User = await database.users.findOne({
+    _id: new ObjectId(decoded._id)
+  })
+
+  const minutesRemaining = (decoded.exp - new Date().getTime() / 1000) / 60
+
+  if (minutesRemaining <= 5) {
+    user['token'] = await generateJWT(new ObjectId(decoded._id))
+  }
+
+  return user
+}
