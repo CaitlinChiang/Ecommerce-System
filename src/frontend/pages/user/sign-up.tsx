1be27127ff318f47467c2953f739619a764de6d6
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-07 00:36:20 +0800

    Export sign-ups for admin and customer

---
+++ src/frontend/pages/user/sign-up.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+import layout from '../../layouts'
+import CreateUser from '../../components/users/Create'
+import { UserType } from 'frontend/_enums/userType'
+
+const Page = (): ReactElement => {
+  return (
+    <>
+      <CreateUser type={UserType.CUSTOMER} />
+    </>
+  )
+}
+
+export default layout(Page, { title: 'Sign Up' })
