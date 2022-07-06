4ba441ba668527bea1036de8d158f26eb5387b08
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-07 00:34:48 +0800

    Create SignInUser component

---
+++ src/frontend/components/users/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import { ReactElement, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useMutation } from '@apollo/client'
+import mutation from './mutation'
+import { Button } from '@mui/material'
+import Text from '../../_common/TextField'
+import PasswordField from '../../_common/PasswordField'
+
+const SignInUser = (): ReactElement => {
+  const router = useRouter()
+
+  const [args, setArgs] = useState<any>({
+    email: null,
+    password: null
+  })
+
+  const [signInMutation, signInMutationState] = useMutation(mutation, {
+    variables: args,
+    onCompleted: () => {
+      console.log('User successfully signed in!')
+      router.push('/')
+    },
+    onError: (error) => console.log(error)
+  })
+
+  return (
+    <>
+      <Text args={args} required={true} setArgs={setArgs} targetProp={'email'} />
+      <PasswordField args={args} required={true} setArgs={setArgs} />
+      <Button
+        onClick={() => signInMutation()}
+        disabled={signInMutationState.loading}
+      >
+        {'Sign In'}
+      </Button>
+    </>
+  )
+}
+
+export default SignInUser
