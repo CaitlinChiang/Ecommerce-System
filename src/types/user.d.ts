08fd7bd427a65230c5a6a7414cf3b27baa69aea3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-07 00:35:53 +0800

    type is UserType in User type

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'mongodb'
 import { PaginateDataArgs } from './actions/paginateData'
 import { DeliveryAddress } from './common/deliveryAddress'
+import { UserType } from '../backend/_enums/userType'
 
 export interface User {
   _id?: ObjectId
@@ -12,7 +13,7 @@ export interface User {
   password?: string
   phoneNumber?: string
   token?: string
-  type?: string
+  type?: UserType
   createdAt?: Date
   updatedAt?: Date
   deletedAt?: Date
@@ -31,18 +32,18 @@ export interface GetUserArgs {
   _id?: ObjectId
   active?: boolean
   paginateData?: PaginateDataArgs
-  type?: string
+  type?: UserType
 }
 
 export interface CreateUserArgs {
-  active: boolean
+  active?: boolean
   deliveryAddress?: DeliveryAddress
   email: string
   firstName: string
   lastName: string
   password: string
   phoneNumber: string
-  type: string
+  type: UserType
   createdAt?: Date
 }
 
