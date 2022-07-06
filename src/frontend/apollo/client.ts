662bc2836bd87f8b07b3ea6d2dd1ea12c6704845
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-07 03:03:13 +0800

    Add authLink to concat with uploadLink in apollo server

--- src/frontend/apollo/client.ts
+++ src/frontend/apollo/client.ts
@@ -1,15 +1,25 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client'
+import { setContext } from '@apollo/link-context'
 import Cookies from 'js-cookie'
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: createUploadLink({
-    uri: '/graphql'
-  }),
-  cache: new InMemoryCache(),
-  headers: {
-    accessToken: Cookies.get('accessToken')
+const authLink = setContext((_, { headers }) => {
+  const accessToken = Cookies.get('accessToken')
+  return {
+    headers: { ...headers, accessToken }
   }
+})
+
+const uploadLink = createUploadLink({ uri: '/graphql' })
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  link: ApolloLink.from([authLink, uploadLink]),
+  cache: new InMemoryCache()
 })
 
 export default client
