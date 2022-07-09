5ad80e372471843116e42ea90b4a79850ee83ef6
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-10 02:00:06 +0800

    Add resolvers to analytics & fix gql error

--- src/backend/controllers/analytics/index.ts
+++ src/backend/controllers/analytics/index.ts
@@ -1,9 +1,11 @@
 import * as typeDefs from './typeDefs.graphql'
+import resolvers from './resolvers'
 import queries from './queries'
 
 export default {
   typeDefs,
   resolvers: {
+    ...resolvers,
     Query: queries
   }
 }
