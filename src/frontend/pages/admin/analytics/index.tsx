9a54f16b713f2b194e5ec5a6b7293043b0af09be
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-10 02:09:30 +0800

    Export revenue chart

--- src/frontend/pages/admin/analytics/index.tsx
+++ src/frontend/pages/admin/analytics/index.tsx
@@ -1,11 +1,13 @@
 import { ReactElement } from 'react'
 import layout from '../../../layouts'
 import OrdersChart from '../../../components/analytics/Showcase/orders/chart'
+import RevenueChart from '../../../components/analytics/Showcase/revenue/chart'
 
 const Page = (): ReactElement => {
   return (
     <>
       <OrdersChart />
+      <RevenueChart />
     </>
   )
 }
