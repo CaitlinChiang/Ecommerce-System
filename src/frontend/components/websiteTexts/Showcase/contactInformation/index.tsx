964673c89b2954ef196859e60d27a7ff65e99f61
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-14 22:24:33 +0800

    Create ContactInformation showcase

---
+++ src/frontend/components/websiteTexts/Showcase/contactInformation/index.tsx
@@ -0,0 +1,57 @@
+import { ReactElement, useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import query from '../../Showcase/query'
+import { Link, Typography } from '@mui/material'
+import { WebsiteText } from '../../../../../types/websiteText'
+import { WebsiteTextType } from '../../../../_enums/websiteTextType'
+import { formatContactInformation } from '../../../../_utils/handleFormatting/formatContactInformation'
+
+const ContactInformation = (): ReactElement => {
+  const [args, setArgs] = useState<any>({
+    email: null,
+    facebook: null,
+    instagram: null,
+    phoneNumber: null
+  })
+
+  const { data } = useQuery(query, {
+    variables: { type: WebsiteTextType.CONTACT_INFORMATION }
+  })
+
+  const websiteText: WebsiteText = data?.get_website_text || {}
+
+  useEffect(() => {
+    setArgs({
+      facebook: formatContactInformation('Facebook', websiteText),
+      instagram: formatContactInformation('Instagram', websiteText),
+      email: formatContactInformation('Email', websiteText),
+      phoneNumber: formatContactInformation('PhoneNumber', websiteText)
+    })
+  }, [data])
+
+  return (
+    <>
+      <Typography variant={'h4'} sx={{ marginBottom: 4 }}>
+        {'Contact Information'}
+      </Typography>
+      {args?.facebook && (
+        <Typography variant={'h6'} sx={{ display: 'inline-block', marginRight: 5 }}>
+          <Link href={args?.facebook}>Facebook</Link>
+        </Typography>
+      )}
+      {args?.instagram && (
+        <Typography variant={'h6'} sx={{ display: 'inline-block', marginRight: 5 }}>
+          <Link href={args?.instagram}>Instagram</Link>
+        </Typography>
+      )}
+      <Typography variant={'h6'} sx={{ display: 'inline-block', marginRight: 5 }}>
+        {args.email}
+      </Typography>
+      <Typography variant={'h6'} sx={{ display: 'inline-block', marginRight: 5 }}>
+        {args.phoneNumber}
+      </Typography>
+    </>
+  )
+}
+
+export default ContactInformation
