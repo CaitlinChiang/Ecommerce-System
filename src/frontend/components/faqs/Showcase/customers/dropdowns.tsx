9594a3e9aef86b1e0fd748fddce0ca3bb3168119
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-14 23:15:24 +0800

    Create FAQsDropwdowns for customers

---
+++ src/frontend/components/faqs/Showcase/customers/dropdowns.tsx
@@ -0,0 +1,59 @@
+import { ReactElement, useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { queryMultiple } from '../query'
+import { Typography } from '@mui/material'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'
+import { FAQ } from '../../../../../types/faq'
+import { PaginateDataArgs } from '../../../../../types/actions/paginateData'
+import { SortDirection } from '../../../../_enums/sortDirection'
+import DropdownsComponent from '../../../_common/DropdownsComponent'
+import { fetchMoreArgs } from '../../../../_utils/handleArgs/returnFetchMoreArgs'
+
+const FAQsDropdowns = (): ReactElement => {
+  const args: any = {}
+  const [paginateDataArgs, setPaginateDataArgs] = useState<PaginateDataArgs>({
+    page: 0,
+    rowsPerPage: 10,
+    searchText: '',
+    sortBy: 'createdAt',
+    sortDirection: SortDirection.ASC
+  })
+
+  const { data, loading, fetchMore } = useQuery(queryMultiple, {
+    variables: { ...args, paginateData: paginateDataArgs },
+    ...fetchMoreArgs
+  })
+
+  const faqs = data?.get_faqs || []
+  const faqsCount: number = data?.get_faqs_count || 0
+
+  const faqRows = faqs?.map((faq: FAQ): any => {
+    return {
+      title: faq.question,
+      content: <Typography>{faq.answer}</Typography>
+    }
+  })
+
+  const icons = {
+    closed: <AddCircleIcon />,
+    opened: <RemoveCircleIcon />
+  }
+
+  return (
+    <>
+      <DropdownsComponent
+        args={args}
+        count={faqsCount}
+        fetchMore={fetchMore}
+        icons={icons}
+        loading={loading}
+        paginateDataArgs={paginateDataArgs}
+        rows={faqRows}
+        setPaginateDataArgs={setPaginateDataArgs}
+      />
+    </>
+  )
+}
+
+export default FAQsDropdowns
