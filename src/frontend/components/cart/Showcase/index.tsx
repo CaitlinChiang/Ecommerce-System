a17b0c96c86b564bdc0d4c5dc9986ce99777e2e2
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-16 00:24:13 +0800

    Create Cart component

---
+++ src/frontend/components/cart/Showcase/index.tsx
@@ -0,0 +1,90 @@
+import { ReactElement } from 'react'
+import { useRouter } from 'next/router'
+import theme from '../../../themes'
+import { useQuery } from '@apollo/client'
+import query from './query'
+import { Box, Button, Container, Divider, Typography } from '@mui/material'
+import { CartItem } from '../../../../types/cart'
+import EditQuantity from '../EditQuantity'
+
+const Cart = (): ReactElement => {
+  const router = useRouter()
+
+  const { data, refetch } = useQuery(query)
+
+  const cart = data?.get_cart || {}
+
+  return (
+    <>
+      <Container>
+        {cart?.items?.map((cartItem: CartItem): ReactElement => {
+          return (
+            <>
+              <Box
+                component='img'
+                sx={{
+                  height: 233,
+                  width: 350,
+                  maxHeight: { xs: 233, md: 167 },
+                  maxWidth: { xs: 350, md: 250 }
+                }}
+                alt='The house from the offer.'
+                src={
+                  cartItem?.productVariant?.imageUrl || cartItem?.product?.imageUrl
+                }
+              />
+              <Typography variant={'h6'}>{cartItem?.product?.name}</Typography>
+              <Typography variant={'body1'}>
+                {cartItem?.productVariant?.name}
+              </Typography>
+              <Typography variant={'h6'}>
+                {'P' + cartItem?.totalPrice?.toFixed(2)}
+              </Typography>
+              <EditQuantity
+                productId={cartItem?.productId}
+                productVariantId={cartItem?.productVariantId}
+                quantity={cartItem?.quantity}
+                refetch={refetch}
+              />
+              <Divider />
+            </>
+          )
+        })}
+        <Typography variant={'h6'}>{'Total  P' + cart?.totalPrice}</Typography>
+      </Container>
+      <Container>
+        <Typography variant={'h6'}>{'Total'}</Typography>
+        <Typography variant={'h6'}>{'Quantity ' + cart?.quantity}</Typography>
+        <Typography variant={'h6'}>{'Amount Due ' + cart?.totalPrice}</Typography>
+        <Button
+          color={'primary'}
+          onClick={(): void => {
+            router.push('/payment')
+          }}
+          sx={{
+            [theme.breakpoints.down('sm')]: {
+              marginTop: theme.spacing(1)
+            }
+          }}
+        >
+          {'Proceed to Checkout'}
+        </Button>
+        <Button
+          color={'primary'}
+          onClick={(): void => {
+            router.push('/shop')
+          }}
+          sx={{
+            [theme.breakpoints.down('sm')]: {
+              marginTop: theme.spacing(1)
+            }
+          }}
+        >
+          {'Continue Shopping'}
+        </Button>
+      </Container>
+    </>
+  )
+}
+
+export default Cart
