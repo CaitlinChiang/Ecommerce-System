0d6ac47ae1dc13d22d563d79411e744a06864776
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-16 00:23:57 +0800

    Create EditQUantity component

---
+++ src/frontend/components/cart/EditQuantity/index.tsx
@@ -0,0 +1,39 @@
+import { ReactElement } from 'react'
+import { useMutation } from '@apollo/client'
+import mutation from './mutation'
+import { ObjectId } from 'mongodb'
+import NumberIncrementor from '../../_common/NumberIncrementor'
+
+const EditQuantity = ({
+  productId,
+  productVariantId,
+  quantity,
+  refetch
+}: {
+  productId?: ObjectId
+  productVariantId?: ObjectId
+  quantity: number
+  refetch: any
+}): ReactElement => {
+  const args: any = {
+    productId,
+    productVariantId,
+    quantity
+  }
+
+  const updateMutation = useMutation(mutation, {
+    onCompleted: () => refetch()
+  })
+
+  return (
+    <>
+      <NumberIncrementor
+        args={args}
+        targetProp={'quantity'}
+        updateMutation={updateMutation}
+      />
+    </>
+  )
+}
+
+export default EditQuantity
