3fd2adb8d3bfc5bb3c1ab4c889a9674316ba61f1
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-15 17:41:01 +0800

    Create CardDisplay for when cards need redirecting

--- src/frontend/components/_common/CardComponent.tsx
+++ src/frontend/components/_common/CardComponent.tsx
@@ -1,38 +1,73 @@
 import { ReactElement } from 'react'
+import { useRouter } from 'next/router'
 import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material'
 
 const CardComponent = ({
   content,
   imageAlt,
   imageSource,
+  redirectLink,
   width
 }: {
   content: ReactElement
   imageAlt?: string
   imageSource?: string
+  redirectLink?: any
   width?: number
 }): ReactElement => {
+  const router = useRouter()
+
+  const CardDisplay = ({ content }: { content: ReactElement }): ReactElement => {
+    if (redirectLink) {
+      return (
+        <Card
+          sx={{
+            width: width || 250,
+            marginTop: 5,
+            marginRight: 5,
+            display: 'inline-block'
+          }}
+          onClick={(): void => {
+            if (redirectLink) router.push(redirectLink)
+          }}
+        >
+          {content}
+        </Card>
+      )
+    }
+
+    if (!redirectLink) {
+      return (
+        <Card
+          sx={{
+            width: width || 250,
+            marginTop: 5,
+            marginRight: 5,
+            display: 'inline-block'
+          }}
+        >
+          {content}
+        </Card>
+      )
+    }
+  }
+
   return (
-    <Card
-      sx={{
-        width: width || 250,
-        marginTop: 5,
-        marginRight: 5,
-        display: 'inline-block'
-      }}
-    >
-      <CardActionArea>
-        {imageSource?.length > 0 && (
-          <CardMedia
-            alt={imageAlt}
-            component={'img'}
-            height={'200'}
-            image={imageSource}
-          />
-        )}
-        <CardContent>{content}</CardContent>
-      </CardActionArea>
-    </Card>
+    <CardDisplay
+      content={
+        <CardActionArea>
+          {imageSource?.length > 0 && (
+            <CardMedia
+              alt={imageAlt}
+              component={'img'}
+              height={'200'}
+              image={imageSource}
+            />
+          )}
+          <CardContent>{content}</CardContent>
+        </CardActionArea>
+      }
+    />
   )
 }
 
