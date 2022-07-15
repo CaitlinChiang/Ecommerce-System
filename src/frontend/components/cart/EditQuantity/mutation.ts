d6bb182f99b8360965c1e050fe5440f6f6158f52
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-16 00:24:06 +0800

    Add mutation fields

--- src/frontend/components/cart/EditQuantity/mutation.ts
+++ src/frontend/components/cart/EditQuantity/mutation.ts
@@ -1,8 +1,12 @@
 import { gql } from '@apollo/client'
 
 export default gql`
-  mutation ($productId: ID, $productVariantId: ID, quantity: Int!) {
-    edit_item_quantity(productId: $productId, productVariantId: $productVariantId) {
+  mutation ($productId: ID, $productVariantId: ID, $quantity: Int!) {
+    edit_item_quantity(
+      productId: $productId
+      productVariantId: $productVariantId
+      quantity: $quantity
+    ) {
       _id
     }
   }
