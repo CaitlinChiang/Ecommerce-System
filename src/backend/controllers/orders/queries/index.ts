86181a8533561191b05b66ec723e1e2e222e44c5
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-17 22:02:07 +0800

    Remove get_order

--- src/backend/controllers/orders/queries/index.ts
+++ src/backend/controllers/orders/queries/index.ts
@@ -1,9 +1,7 @@
-import get_order from './get_order'
 import get_orders from './get_orders'
 import get_orders_count from './get_orders_count'
 
 export default {
-  get_order,
   get_orders,
   get_orders_count
 }
