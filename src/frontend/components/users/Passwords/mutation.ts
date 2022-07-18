98c887ec6c779534359839f5f47f5ff2675c39b5
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-18 23:24:18 +0800

    Add mutation for password mutations

---
+++ src/frontend/components/users/Passwords/mutation.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client'
+
+export const ForgotUserPassword = gql`
+  mutation ($email: String!) {
+    forgot_user_password(email: $email)
+  }
+`
+
+export const ResetUserPassword = gql`
+  mutation ($email: String!, $newPassword: String!, oldPassword: String, verificationCode: String) {
+    reset_user_password(email: $email, newPassword: $newPassword, oldPassword: $oldPassword, verificationCode: $verificationCode) {
+      _id
+    }
+  }
+`
