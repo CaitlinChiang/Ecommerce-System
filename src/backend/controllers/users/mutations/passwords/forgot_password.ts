ec8c3d7ea2b3909437668969fdbee8a672191bd3
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-18 22:37:27 +0800

    Create logic for forgot_password

---
+++ src/backend/controllers/users/mutations/passwords/forgot_password.ts
@@ -0,0 +1,25 @@
+import { Context } from '../../../../../types/setup/context'
+import { ForgotPasswordArgs } from '../../../../../types/user'
+import { MutateAction } from '../../../../_enums/mutateAction'
+import { authenticateUser } from '../../../../_utils/auth/authenticateUser'
+import { mutationArgs } from '../../../../_utils/handleArgs/returnMutationArgs'
+import { checkIfUserExists } from '../../../../_utils/handleValidation/checkIfUserExists'
+
+export default async (
+  _root: undefined,
+  args: ForgotPasswordArgs,
+  context: Context
+): Promise<void> => {
+  authenticateUser({ admin: false }, context)
+
+  await checkIfUserExists(args.email, true, context)
+
+  const verificationCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+  await context.database.users.findOneAndUpdate(
+    { email: args.email },
+    { $set: mutationArgs({ verificationCode }, MutateAction.UPDATE) }
+  )
+
+  // ADD FORGOT PASSWORD EMAIL CONTAINING VERIFICATION CODE
+}
