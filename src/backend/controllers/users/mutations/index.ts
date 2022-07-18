c8b19b82dbfe837035ad8b5e6397491ef71a8afa
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-18 23:23:40 +0800

    Refactor name of password mutations

--- src/backend/controllers/users/mutations/index.ts
+++ src/backend/controllers/users/mutations/index.ts
@@ -2,14 +2,14 @@ import sign_in_user from './sign_in_user'
 import create_user from './create_user'
 import update_user from './update_user'
 import delete_user from './delete_user'
-import forgot_password from './passwords/forgot_password'
-import reset_password from './passwords/reset_password'
+import forgot_user_password from './passwords/forgot_user_password'
+import reset_user_password from './passwords/reset_user_password'
 
 export default {
   sign_in_user,
   create_user,
   update_user,
   delete_user,
-  forgot_password,
-  reset_password
+  forgot_user_password,
+  reset_user_password
 }
