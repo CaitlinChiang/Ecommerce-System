9c97620f1ad698e37b59d177d4db6d5f31fb413d
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-18 23:25:23 +0800

    Create ResetPassword component

--- src/frontend/components/users/Passwords/resetPassword/fromProfile.tsx
+++ src/frontend/components/users/Passwords/resetPassword/fromProfile.tsx
@@ -1,31 +1,36 @@
 import { ReactElement, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useMutation } from '@apollo/client'
-import { ForgotUserPassword } from '../mutation'
+import { ResetUserPassword } from '../mutation'
 import { Button } from '@mui/material'
 import Text from '../../../_common/TextField'
+import PasswordField from '../../../_common/PasswordField'
 import Notification from '../../../_common/Notification'
 
-const ForgotPassword = (): ReactElement => {
+const ResetPassword = (): ReactElement => {
   const router = useRouter()
 
-  const [args, setArgs] = useState<any>({ email: null })
+  const [args, setArgs] = useState<any>({
+    email: null,
+    newPassword: null,
+    oldPassword: null
+  })
   const [validateFields, setValidateFields] = useState<boolean>(false)
   const [notification, setNotification] = useState<any>({
     message: null,
     success: null
   })
 
-  const [forgotPasswordMutation, forgotPasswordMutationState] = useMutation(
-    ForgotUserPassword,
+  const [resetPasswordMutation, resetPasswordMutationState] = useMutation(
+    ResetUserPassword,
     {
       variables: args,
       onCompleted: () => {
         setNotification({
-          message: 'Please check your email for your verification code.',
+          message: 'Password successfully updated!',
           success: true
         })
-        router.push('/user/reset-password/verify')
+        router.push('/user/profile')
       },
       onError: (error) => setNotification({ message: error.message, success: false })
     }
@@ -40,18 +45,32 @@ const ForgotPassword = (): ReactElement => {
         setArgs={setArgs}
         targetProp={'email'}
       />
+      <PasswordField
+        args={args}
+        error={validateFields}
+        required={true}
+        setArgs={setArgs}
+        targetProp={'newPassword'}
+      />
+      <PasswordField
+        args={args}
+        error={validateFields}
+        required={true}
+        setArgs={setArgs}
+        targetProp={'oldPassword'}
+      />
       <Button
         onClick={() => {
           setValidateFields(true)
-          forgotPasswordMutation()
+          resetPasswordMutation()
         }}
-        disabled={forgotPasswordMutationState.loading}
+        disabled={resetPasswordMutationState.loading}
       >
-        {'Get Verification Code'}
+        {'Forgot Password'}
       </Button>
       <Notification message={notification.message} success={notification.success} />
     </>
   )
 }
 
-export default ForgotPassword
+export default ResetPassword
