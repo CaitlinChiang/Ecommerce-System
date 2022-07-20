bcd6ffacc44dbca259793962676516b081899e83
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-20 20:07:53 +0800

    Fix errors

--- src/backend/controllers/products/queries/get_product.ts
+++ src/backend/controllers/products/queries/get_product.ts
@@ -1,14 +1,14 @@
 import { ObjectId } from 'mongodb'
-import { authenticateUser } from '../../../_utils/auth/authenticateUser'
 import { Context } from '../../../../types/setup/context'
 import { Product, GetProductArgs } from '../../../../types/product'
+import { authenticateUser } from '../../../_utils/auth/authenticateUser'
 
 export default async (
   _root: undefined,
   args: GetProductArgs,
   context: Context
 ): Promise<Product> => {
-  authenticateUser({ admin: false }, context)
+  authenticateUser({ admin: false, context })
 
   const product: Product = await context.database.products.findOne({
     _id: new ObjectId(args._id)
