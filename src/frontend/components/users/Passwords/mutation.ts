c629203dbf12cdccf3c72c819e7295849a4827fa
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-21 12:30:13 +0800

    Fix mutation spelling error

--- src/frontend/components/users/Passwords/mutation.ts
+++ src/frontend/components/users/Passwords/mutation.ts
@@ -7,8 +7,18 @@ export const ForgotUserPassword = gql`
 `
 
 export const ResetUserPassword = gql`
-  mutation ($email: String!, $newPassword: String!, oldPassword: String, verificationCode: String) {
-    reset_user_password(email: $email, newPassword: $newPassword, oldPassword: $oldPassword, verificationCode: $verificationCode) {
+  mutation (
+    $email: String!
+    $newPassword: String!
+    $oldPassword: String
+    $verificationCode: String
+  ) {
+    reset_user_password(
+      email: $email
+      newPassword: $newPassword
+      oldPassword: $oldPassword
+      verificationCode: $verificationCode
+    ) {
       _id
     }
   }
