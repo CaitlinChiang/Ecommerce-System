91a7d7e5151eaeb54c32fbbd00522cd2e171b880
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-21 12:28:55 +0800

    Add sign up and forgot password routs to sign in component

--- src/frontend/components/users/SignIn/index.tsx
+++ src/frontend/components/users/SignIn/index.tsx
@@ -4,17 +4,19 @@ import { useRouter } from 'next/router'
 import { useMutation } from '@apollo/client'
 import mutation from './mutation'
 import { Button } from '@mui/material'
+import { UserType } from '../../../_enums/userType'
 import Text from '../../_common/TextField'
 import PasswordField from '../../_common/PasswordField'
 
 const globalAny: any = global
 
-const SignInUser = (): ReactElement => {
+const SignInUser = ({ type }: { type: UserType }): ReactElement => {
   const router = useRouter()
 
   const [args, setArgs] = useState<any>({
     email: null,
-    password: null
+    password: null,
+    type
   })
   const [validateFields, setValidateFields] = useState<boolean>(false)
 
@@ -52,6 +54,26 @@ const SignInUser = (): ReactElement => {
         }}
       >
         {'Sign In'}
+      </Button>
+      <Button
+        disabled={signInMutationState.loading}
+        onClick={(): void => {
+          router.push(
+            `${type === UserType.ADMINISTRATOR ? '/admin' : ''}/user/sign-up`
+          )
+        }}
+      >
+        {'Sign Up'}
+      </Button>
+      <Button
+        disabled={signInMutationState.loading}
+        onClick={(): void => {
+          router.push(
+            `${type === UserType.ADMINISTRATOR ? '/admin' : ''}/user/forgot-password`
+          )
+        }}
+      >
+        {'Forgot Password'}
       </Button>
     </>
   )
