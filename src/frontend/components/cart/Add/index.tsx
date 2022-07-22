47b338eea7a06602f0eba8279bb14ff4e7b4e9e6
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-22 18:16:57 +0800

    Create AddCartItem frontend component

---
+++ src/frontend/components/cart/Add/index.tsx
@@ -0,0 +1,61 @@
+import { ReactElement, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useQuery, useMutation } from '@apollo/client'
+import { GetUser } from '../../users/Showcase/query'
+import mutation from './mutation'
+import { AddCartItemArgs } from '../../../../types/cart'
+import { User } from '../../../../types/user'
+import { Button } from '@mui/material'
+import ModalComponent from '../../_common/ModalComponent'
+
+const globalAny: any = global
+
+const AddCartItem = ({ args }: { args: AddCartItemArgs }): ReactElement => {
+  const router = useRouter()
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  const { data } = useQuery(GetUser)
+
+  const user: User = data?.get_user || {}
+
+  const [updateMutation, updateMutationState] = useMutation(mutation, {
+    variables: args,
+    onCompleted: () => {
+      globalAny.setNotification(true, 'Item added to cart!')
+    }
+  })
+
+  return (
+    <>
+      <ModalComponent
+        content={
+          <Button
+            onClick={(): void => {
+              router.push('/user/sign-in')
+            }}
+          >
+            {'Sign In'}
+          </Button>
+        }
+        onClose={(): void => setModalOpen(false)}
+        open={modalOpen}
+        title={'Please sign in to add items to cart.'}
+      />
+      <Button
+        disabled={updateMutationState.loading}
+        onClick={(): void => {
+          if (Object.keys(user).length === 0) {
+            setModalOpen(true)
+          } else {
+            updateMutation()
+          }
+        }}
+      >
+        {'Add to Cart'}
+      </Button>
+    </>
+  )
+}
+
+export default AddCartItem
