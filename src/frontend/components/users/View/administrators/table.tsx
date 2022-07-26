fb5911232f1701cf720fff52fc86e095f030e7ef
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-26 23:48:23 +0800

    Add adminPermissions component to table

--- src/frontend/components/users/View/administrators/table.tsx
+++ src/frontend/components/users/View/administrators/table.tsx
@@ -2,13 +2,15 @@ import { ReactElement, useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { GetUsers } from '../query'
 import deleteMutation from '../../Delete/mutation'
-import { TableCell, TableRow } from '@mui/material'
+import { Button, TableCell, TableRow } from '@mui/material'
 import { User } from '../../../../../types/user'
 import { PaginateDataArgs } from '../../../../../types/actions/paginateData'
 import { RefetchDataArgs } from '../../../../../types/actions/refetchData'
 import { SortDirection } from '../../../../_enums/sortDirection'
 import { UserType } from '../../../../_enums/userType'
 import TableComponent from '../../../_common/TableComponent'
+import ModalComponent from '../../../_common/ModalComponent'
+import UpdateAdminPermissions from '../../Update/adminPermissions'
 import UpdateUserCheckbox from '../../Update/checkbox'
 import DeleteButton from '../../../_common/DeleteButton'
 import AdministratorsTableFilters from './tableFilters'
@@ -26,6 +28,8 @@ const AdministratorsTable = (): ReactElement => {
     sortBy: 'createdAt',
     sortDirection: SortDirection.DESC
   })
+  const [user, setUser] = useState<any>({ _id: null, permissions: [] })
+  const [permissionsModalOpen, setPermissionsModalOpen] = useState<boolean>(false)
   const [filterOpen, setFilterOpen] = useState<boolean>(false)
   const [refetchArgs, setRefetchArgs] = useState<RefetchDataArgs>({
     args: null,
@@ -59,6 +63,7 @@ const AdministratorsTable = (): ReactElement => {
     { label: 'lastName', sortable: true },
     { label: 'email', sortable: true },
     { label: 'phoneNumber', sortable: false },
+    { label: 'permissions', sortable: false },
     { label: 'createdAt', sortable: true },
     { label: 'actions', sortable: false }
   ]
@@ -78,6 +83,16 @@ const AdministratorsTable = (): ReactElement => {
           <TableCell>{user?.lastName}</TableCell>
           <TableCell>{user?.email}</TableCell>
           <TableCell>{user?.phoneNumber}</TableCell>
+          <TableCell>
+            <Button
+              onClick={(): void => {
+                setUser({ _id: String(user._id), permissions: user?.permissions })
+                setPermissionsModalOpen(true)
+              }}
+            >
+              {'View Permissions'}
+            </Button>
+          </TableCell>
           <TableCell>{String(user?.createdAt)}</TableCell>
           <TableCell>
             <DeleteButton
@@ -94,21 +109,31 @@ const AdministratorsTable = (): ReactElement => {
   ]
 
   return (
-    <TableComponent
-      args={args}
-      count={usersCount}
-      fetchMore={fetchMore}
-      filterContent={<AdministratorsTableFilters args={args} setArgs={setArgs} />}
-      filterOpen={filterOpen}
-      headers={userHeaders}
-      loading={loading}
-      paginateDataArgs={paginateDataArgs}
-      rows={userRows}
-      searchLabel={'Search Account by Email or Name'}
-      searchPlaceholder={'ex. ava_cruz@gmail.com or Ava Cruz'}
-      setFilterOpen={setFilterOpen}
-      setPaginateDataArgs={setPaginateDataArgs}
-    />
+    <>
+      <ModalComponent
+        content={
+          <UpdateAdminPermissions _id={user._id} permissions={user.permissions} />
+        }
+        onClose={(): void => setPermissionsModalOpen(false)}
+        open={permissionsModalOpen}
+        title={'Admin Permissions'}
+      />
+      <TableComponent
+        args={args}
+        count={usersCount}
+        fetchMore={fetchMore}
+        filterContent={<AdministratorsTableFilters args={args} setArgs={setArgs} />}
+        filterOpen={filterOpen}
+        headers={userHeaders}
+        loading={loading}
+        paginateDataArgs={paginateDataArgs}
+        rows={userRows}
+        searchLabel={'Search Account by Email or Name'}
+        searchPlaceholder={'ex. ava_cruz@gmail.com or Ava Cruz'}
+        setFilterOpen={setFilterOpen}
+        setPaginateDataArgs={setPaginateDataArgs}
+      />
+    </>
   )
 }
 
