c3a710c34dd400d093d223c9937910eb3c073632
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-27 21:43:57 +0800

    Create functions under formatAdminPermissions

--- src/frontend/_utils/handleFormatting/formatAdminPermissions.ts
+++ src/frontend/_utils/handleFormatting/formatAdminPermissions.ts
@@ -19,3 +19,61 @@ export const returnAdminPermissions = (): { [key: string]: string[] } => {
 export const formatPermissionCategory = (permission: string): string => {
   return permission.replaceAll('_', ' ')
 }
+
+export const allCategoryPermissions = (
+  category: string,
+  permissions: string[]
+): boolean => {
+  const adminPermissions: string[] = generatePermissions(category)
+
+  const allPermissions = adminPermissions.every((e) => {
+    return permissions.includes(e)
+  })
+
+  return allPermissions
+}
+
+export const checkAllPermissions = (
+  category: string,
+  permissions: string[]
+): string[] => {
+  const adminPermissions: string[] = []
+
+  Object.keys(AdminPermission).map((permission: string) => {
+    if (
+      permission.includes(category) &&
+      !permission.includes(category + '_') &&
+      !permissions.includes(permission) &&
+      Object(AdminPermission)[permission].length > 0
+    ) {
+      adminPermissions.push(permission)
+    }
+  })
+
+  return adminPermissions
+}
+
+export const uncheckAllPermissions = (
+  category: string,
+  permissions: string[]
+): string[] => {
+  const adminPermissions: string[] = generatePermissions(category)
+
+  return permissions.filter((e) => !adminPermissions.includes(e))
+}
+
+export const generatePermissions = (category: string): string[] => {
+  const adminPermissions: string[] = []
+
+  Object.keys(AdminPermission).map((permission: string) => {
+    if (
+      permission.includes(category) &&
+      !permission.includes(category + '_') &&
+      Object(AdminPermission)[permission].length > 0
+    ) {
+      adminPermissions.push(permission)
+    }
+  })
+
+  return adminPermissions
+}
