788b58a90cc7a636025889a1ebb44a3e76c0f00e
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-27 22:28:01 +0800

    Add authenticateUser logic

--- src/backend/_utils/auth/authenticateUser.ts
+++ src/backend/_utils/auth/authenticateUser.ts
@@ -1,14 +1,21 @@
-import { AuthenticationError } from 'apollo-server-express'
+import { UserInputError } from 'apollo-server-express'
+import { AdminPermission } from '../../_enums/adminPermission'
 import { Context } from '../../../types/setup/context'
 
 export const authenticateUser = async ({
   admin,
+  permission,
   context
 }: {
   admin: boolean
+  permission?: AdminPermission
   context: Context
 }): Promise<void> => {
-  if (admin && !context.currentUserActive) {
-    throw new AuthenticationError('Action not permitted.')
+  const inactiveAdmin = admin && !context.currentUserActive
+  const restrictedAdmin =
+    admin && permission && !context.currentUserPermissions.includes(permission)
+
+  if (inactiveAdmin || restrictedAdmin) {
+    throw new UserInputError('Action not permitted.')
   }
 }
