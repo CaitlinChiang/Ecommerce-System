d84947e80e7b5cf054a89e9d8ffe11fff8328eca
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-27 23:47:22 +0800

    Create authenticateUser in frontend

---
+++ src/frontend/_utils/auth/authenticateUser.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@apollo/client'
+import { GetUser } from '../../layouts/query'
+import { User } from '../../../types/user'
+import { AdminPermission } from '../../_enums/adminPermission'
+
+export const authenticateUser = (permission: AdminPermission): boolean => {
+  const { data } = useQuery(GetUser)
+
+  const user: User = data?.get_user || {}
+
+  if (!user?.permissions.includes(Object(AdminPermission)[permission])) return false
+  return true
+}
--- src/frontend/layouts/query.ts
+++ src/frontend/layouts/query.ts
@@ -8,6 +8,7 @@ export const GetUser = gql`
       email
       firstName
       lastName
+      permissions
       type
     }
   }
