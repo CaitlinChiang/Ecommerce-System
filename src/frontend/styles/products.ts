388aeab99040ce38f7788c3301da9ceb278905a7
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-28 10:46:56 +0800

    Styling

--- src/frontend/styles/products.ts
+++ src/frontend/styles/products.ts
@@ -1,4 +1,10 @@
 const styles = {
+  image: {
+    height: 233,
+    width: 350,
+    maxHeight: { xs: 233, md: 167 },
+    maxWidth: { xs: 350, md: 250 }
+  },
   listItemButton: {
     py: 0,
     minHeight: 32,
