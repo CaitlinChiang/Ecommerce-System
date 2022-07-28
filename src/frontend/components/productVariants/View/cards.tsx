27f73b6a277829429e0870fc8e550896e990486d
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-28 10:46:50 +0800

    Create productVariantCards

---
+++ src/frontend/components/productVariants/View/cards.tsx
@@ -0,0 +1,41 @@
+import { ReactElement } from 'react'
+import { useQuery } from '@apollo/client'
+import { GetProductVariants } from './query'
+import { Typography } from '@mui/material'
+import { ObjectId } from 'mongodb'
+import { ProductVariant } from '../../../../types/productVariant'
+import { SortDirection } from '../../../_enums/sortDirection'
+import CardComponent from '../../_common/CardComponent'
+
+const ProductVariantCards = ({
+  _productId
+}: {
+  _productId: ObjectId
+}): ReactElement => {
+  const args: any = { _productId, showPublic: true }
+
+  const { data } = useQuery(GetProductVariants, {
+    variables: {
+      ...args,
+      paginateData: { sortBy: 'name', sortDirection: SortDirection.ASC }
+    }
+  })
+
+  const productVariants: ProductVariant[] = data?.get_product_variants || []
+
+  const ProductVariantCards = [
+    productVariants?.map((productVariant: ProductVariant): ReactElement => {
+      return (
+        <CardComponent
+          content={<Typography>{productVariant?.name}</Typography>}
+          imageAlt={`${productVariant?.name} Product Variant Image`}
+          imageSource={productVariant?.imageUrl}
+        />
+      )
+    })
+  ]
+
+  return <>{ProductVariantCards}</>
+}
+
+export default ProductVariantCards
