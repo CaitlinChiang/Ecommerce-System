45b1d5ce8affaa8cb5a3172221e43052aa9a1cb9
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-29 00:03:26 +0800

    if provided productVariantid, the card clicked setStates

--- src/frontend/components/_common/CardComponent.tsx
+++ src/frontend/components/_common/CardComponent.tsx
@@ -3,29 +3,31 @@ import { useRouter } from 'next/router'
 import styles from '../../styles/_common/cardComponent'
 import { Card, CardActionArea, CardContent, CardMedia } from '@mui/material'
 
+const globalAny: any = global
+
 const CardComponent = ({
   content,
   imageAlt,
   imageSource,
+  productVariantId,
   redirectLink,
   width
 }: {
   content: ReactElement
   imageAlt?: string
   imageSource?: string
+  productVariantId?: string
   redirectLink?: { path: string; url: string }
   width?: number
 }): ReactElement => {
   const router = useRouter()
 
   const CardDisplay = ({ content }: { content: ReactElement }): ReactElement => {
-    if (redirectLink) {
+    if (productVariantId) {
       return (
         <Card
           sx={{ ...styles.card, width: width || 250 }}
-          onClick={(): void => {
-            router.push(redirectLink.path, redirectLink.url)
-          }}
+          onClick={(): void => globalAny.setProductVariantId(productVariantId)}
         >
           {content}
         </Card>
@@ -35,6 +37,17 @@ const CardComponent = ({
     if (!redirectLink) {
       return <Card sx={{ ...styles.card, width: width || 250 }}>{content}</Card>
     }
+
+    return (
+      <Card
+        sx={{ ...styles.card, width: width || 250 }}
+        onClick={(): void => {
+          router.push(redirectLink.path, redirectLink.url)
+        }}
+      >
+        {content}
+      </Card>
+    )
   }
 
   return (
