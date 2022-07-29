7f4f6177b1c7b9716903365ed7c9727d9f822e7b
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-07-29 11:31:27 +0800

    Add loading check to authenticateUser

--- src/frontend/_utils/auth/authenticateUser.ts
+++ src/frontend/_utils/auth/authenticateUser.ts
@@ -4,10 +4,13 @@ import { User } from '../../../types/user'
 import { AdminPermission } from '../../_enums/adminPermission'
 
 export const authenticateUser = (permission: AdminPermission): boolean => {
-  const { data } = useQuery(GetUser)
+  const { data, loading } = useQuery(GetUser)
+
+  if (loading) return
 
   const user: User = data?.get_user || {}
 
   if (!user?.permissions?.includes(Object(AdminPermission)[permission])) return false
+
   return true
 }
