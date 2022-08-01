3f4a98b11a92eeae40afa3289bebec0adc757235
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-01 17:00:45 +0800

    Edit logic of validateUser

--- src/backend/_utils/handleValidation/validateUser.ts
+++ src/backend/_utils/handleValidation/validateUser.ts
@@ -1,16 +1,27 @@
 import { UserInputError } from 'apollo-server-express'
 import { Context } from '../../../types/setup/context'
+import { User } from '../../../types/user'
+import { UserType } from '../../_enums/userType'
 
 export const checkIfUserExists = async ({
   email,
   shouldExist,
+  type,
   context
 }: {
   email: string
   shouldExist?: boolean
+  type?: UserType
   context: Context
 }): Promise<void> => {
-  const existingUser = await context.database.users.findOne({ email })
+  const existingUser: User = await context.database.users.findOne({ email })
+
+  const existingCustomer: boolean = existingUser?.type === UserType.CUSTOMER
+  const signInAdmin: boolean = type === UserType.ADMINISTRATOR
+
+  if (signInAdmin && existingCustomer) {
+    throw new UserInputError('User is not an administrator.')
+  }
 
   if (!shouldExist && existingUser) {
     throw new UserInputError('User with email already exists.')
