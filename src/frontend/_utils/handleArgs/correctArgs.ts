ec0bd107f687ff703f5c483beab3a3a81a3857f9
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-01 23:20:04 +0800

    Edit logic of correctArgs

--- src/frontend/_utils/handleArgs/correctArgs.ts
+++ src/frontend/_utils/handleArgs/correctArgs.ts
@@ -1,30 +1,66 @@
 import isEmail from 'validator/lib/isEmail'
 import isMobilePhone from 'validator/lib/isMobilePhone'
 
-export const correctArgs = (args: any): any => {
+export const correctArgs = ({
+  args,
+  requiredArgs
+}: {
+  args: any
+  requiredArgs?: string[]
+}): any => {
   Object.keys(args).forEach((key: string): void => {
-    const val = args[key]
+    if (typeof args[key] === 'string') modifyArgs(args, key)
 
-    if (typeof val === 'string') modifyArgs(key, args)
-
-    if (key === 'email' && args?.email && !isEmail(args?.email)) {
-      args.email = null
-    }
-
-    if (
-      key === 'phoneNumber' &&
-      args?.phoneNumber &&
-      !isMobilePhone(args?.phoneNumber)
-    ) {
-      args.phoneNumber = null
+    if (requiredArgs.includes(key)) {
+      checkField({ args, key })
     }
   })
 
   return args
 }
 
-const modifyArgs = (key: string, obj: any): any => {
-  if (obj[key]?.trim().length === 0) {
-    obj[key] = null
+const modifyArgs = (args: any, key: string): any => {
+  if (args[key]?.trim().length === 0) {
+    args[key] = null
   }
+}
+
+export const checkField = ({
+  args,
+  key,
+  requiredField
+}: {
+  args: any
+  key: string
+  requiredField?: string
+}): any => {
+  const requiredArg: string = requiredField || '_id'
+
+  if (key === 'email' || key === 'password' || key === 'phoneNumber') {
+    validateArgs({ [key]: args?.[key] })
+  }
+
+  if (!args?.[key]) {
+    args[requiredArg] = null
+  }
+
+  return args
+}
+
+export const validateArgs = (args: any): any => {
+  const { email, password, phoneNumber } = args
+
+  if (email && !isEmail(email)) {
+    args.email = null
+  }
+
+  if (password && password?.length < 8) {
+    args.password = null
+  }
+
+  if (phoneNumber && !isMobilePhone(phoneNumber)) {
+    args.phoneNumber = null
+  }
+
+  return args
 }
