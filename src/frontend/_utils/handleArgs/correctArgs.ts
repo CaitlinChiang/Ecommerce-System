b35dd641832be39efa4a36544ce5a0349961da7a
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-02 11:01:01 +0800

    Fix correctArgs function

--- src/frontend/_utils/handleArgs/correctArgs.ts
+++ src/frontend/_utils/handleArgs/correctArgs.ts
@@ -1,19 +1,6 @@
-import isEmail from 'validator/lib/isEmail'
-import isMobilePhone from 'validator/lib/isMobilePhone'
-
-export const correctArgs = ({
-  args,
-  requiredArgs
-}: {
-  args: any
-  requiredArgs?: string[]
-}): any => {
+export const correctArgs = (args: any): any => {
   Object.keys(args).forEach((key: string): void => {
-    if (typeof args[key] === 'string') modifyArgs(args, key)
-
-    if (requiredArgs.includes(key)) {
-      checkField({ args, key })
-    }
+    modifyArgs(args, key)
   })
 
   return args
@@ -23,44 +10,4 @@ const modifyArgs = (args: any, key: string): any => {
   if (args[key]?.trim().length === 0) {
     args[key] = null
   }
-}
-
-export const checkField = ({
-  args,
-  key,
-  required
-}: {
-  args: any
-  key: string
-  required?: string
-}): any => {
-  const requiredArg: string = required || '_id'
-
-  if (key === 'email' || key === 'password' || key === 'phoneNumber') {
-    validateArgs({ [key]: args?.[key] })
-  }
-
-  if (!args?.[key]) {
-    args[requiredArg] = null
-  }
-
-  return args
-}
-
-export const validateArgs = (args: any): any => {
-  const { email, password, phoneNumber } = args
-
-  if (email && !isEmail(email)) {
-    args.email = null
-  }
-
-  if (password && password?.length < 8) {
-    args.password = null
-  }
-
-  if (phoneNumber && !isMobilePhone(phoneNumber)) {
-    args.phoneNumber = null
-  }
-
-  return args
 }
