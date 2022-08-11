aa6259fee37448b0e3036757631b5c7b82746171
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-11 18:06:05 +0800

    Fix audit fields

--- src/types/auditLog.d.ts
+++ src/types/auditLog.d.ts
@@ -1,21 +1,11 @@
 import { ObjectId } from 'mongodb'
 import { PaginateDataArgs } from './actions/paginateData'
 import { DateRange } from './common/dateRange'
-import { WebsiteTextType } from '../backend/_enums/websiteTextType'
 
 export interface AuditLog {
   _id?: ObjectId
   action?: string
-  cityId?: ObjectId
-  faqId?: ObjectId
   orderId?: ObjectId
-  paymentMethodId?: ObjectId
-  productCategoryId?: ObjectId
-  productId?: ObjectId
-  productVariantId?: ObjectId
-  reviewId?: ObjectId
-  userId?: ObjectId
-  websiteTextType?: WebsiteTextType
   createdAt?: Date
   createdBy?: ObjectId
   createdByEmail?: string
