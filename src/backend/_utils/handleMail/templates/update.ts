15ee0efc05107eb29ec564f6bb369d89d340875e
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-17 12:51:16 +0800

    Create update template function

---
+++ src/backend/_utils/handleMail/templates/update.ts
@@ -0,0 +1,14 @@
+const AWS = require('../setup/aws-ses')
+
+// CHANGE IMPORT DEPENDING ON WHICH TEMPLATE IS BEING UPDATED
+import { verificationCodeTemplate } from './htmlTemplates/verificationCode'
+
+export const updateTemplate = async (): Promise<void> => {
+  const updateTemplate = new AWS.SES({ apiVersion: '2010-12-01' })
+    .updateTemplate({ Template: verificationCodeTemplate })
+    .promise()
+
+  updateTemplate
+    .then(() => console.log('Template Successfully Updated!'))
+    .catch((err) => console.error(err, err.stack))
+}
