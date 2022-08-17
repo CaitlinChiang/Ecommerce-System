ca320f216686fd15c1a6840c448e2cb296761837
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-17 12:50:02 +0800

    Create template function

---
+++ src/backend/_utils/handleMail/templates/create.ts
@@ -0,0 +1,14 @@
+const AWS = require('../setup/aws-ses')
+
+// CHANGE IMPORT DEPENDING ON WHICH TEMPLATE IS BEING CREATED
+import { verificationCodeTemplate } from './htmlTemplates/verificationCode'
+
+export const createTemplate = async (): Promise<void> => {
+  const createTemplate = new AWS.SES({ apiVersion: '2010-12-01' })
+    .createTemplate({ Template: verificationCodeTemplate })
+    .promise()
+
+  createTemplate
+    .then(() => console.log('Template Successfully Created!'))
+    .catch((err) => console.error(err, err.stack))
+}
