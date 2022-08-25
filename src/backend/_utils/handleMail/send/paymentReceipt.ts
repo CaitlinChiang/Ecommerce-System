0835e3354eb531fc66adba7f3f8e0ad5f97118bd
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-25 21:25:26 +0800

    Fix paymentReceipt

--- src/backend/_utils/handleMail/send/paymentReceipt.ts
+++ src/backend/_utils/handleMail/send/paymentReceipt.ts
@@ -20,17 +20,21 @@ export const sendPaymentReceipt = async (
 
   const order: Order = await context.database.orders.findOne({ _id: orderId })
   const orderItems: CartItem[] = await returnCartItems(order.items, context)
-  const items = orderItems.map((item: CartItem) => {
-    return {
-      name: item?.productVariant?.name || item?.product?.name,
-      quantity: item.quantity,
-      totalPrice: item.totalPrice
-    }
-  })
   const user: User = await context.database.users.findOne({ _id: order.userId })
   const city: City = await context.database.cities.findOne({
     _id: order.deliveryAddress.cityId
   })
+
+  let items = '['
+
+  for (let i = 0, n = orderItems?.length; i < n; i++) {
+    const cartItem: CartItem = orderItems[i]
+
+    const { quantity, totalPrice } = cartItem
+    const name = cartItem?.productVariant?.name || cartItem?.product?.name
+
+    items += `{ "name": "${name}", "quantity": "${quantity}", "totalPrice": "${totalPrice}" }, `
+  }
 
   const args = {
     Destination: {
@@ -38,35 +42,22 @@ export const sendPaymentReceipt = async (
       ToAddresses: [user.email]
     },
     Template: 'PAYMENT_RECEIPT_TEMPLATE',
-    TemplateData:
-      '{ "orderId": "' +
-      orderId +
-      '", "createdAt": "' +
-      formatDateTime(order.createdAt) +
-      '", "user": { "firstName": "' +
-      user.firstName +
-      '", "lastName": "' +
-      user.lastName +
-      '", "phoneNumber": "' +
-      user.phoneNumber +
-      '", "email": "' +
-      user.email +
-      '" }, "address": "' +
-      order.deliveryAddress.address +
-      '", "city": "' +
-      city.name +
-      '", "items": [], "payment": { "amountDue": "' +
-      payment.amountDue +
-      '", "shippingFee": "' +
-      payment.shippingFee +
-      '", "total": "' +
-      formatPrice(payment.amountDue + payment.shippingFee) +
-      '" } }',
+    TemplateData: `{ "orderId": "${orderId}", "createdAt": "${formatDateTime(
+      order.createdAt
+    )}", "user": { "firstName": "${user.firstName}", "lastName": "${
+      user.lastName
+    }", "phoneNumber": "${user.phoneNumber}", "email": "${
+      user.email
+    }" }, "address": "${order.deliveryAddress.address}", "city": "${
+      city.name
+    }", "items": ${items.slice(0, -2) + ']'}, "payment": { "amountDue": "${
+      payment.amountDue
+    }", "shippingFee": "${payment.shippingFee}", "total": "${formatPrice(
+      payment.amountDue + payment.shippingFee
+    )}" } }`,
     Source: 'estoree.services@gmail.com',
     ReplyToAddresses: []
   }
-
-  // "subscription": [{ "interest": "Sports"}, { "interest": "Travel"}, { "interest": "Cooking" }]}"
 
   const sendEmail = new AWS.SES({ apiVersion: '2010-12-01' })
     .sendTemplatedEmail(args)
