195e44da8d3e7d37745a9ce4532bfba821e67513
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-26 23:23:31 +0800

    Add AuthorizedPath to NoPersmissions component

--- src/frontend/components/_common/NoPermissions.tsx
+++ src/frontend/components/_common/NoPermissions.tsx
@@ -1,8 +1,13 @@
 import { ReactElement } from 'react'
 import { Typography } from '@mui/material'
+import AuthorizedPath from '../users/Authorization'
 
 const NoPermissions = (): ReactElement => {
-  return <Typography>{'No access permissions.'}</Typography>
+  return (
+    <AuthorizedPath>
+      <Typography>{'You have no authorization to access this area.'}</Typography>
+    </AuthorizedPath>
+  )
 }
 
 export default NoPermissions
