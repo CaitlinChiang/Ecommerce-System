d5d6008b50ef704983d678aa5386715df98f4e83
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-27 01:00:59 +0800

    Add AuthorizedPath to analytics

--- src/frontend/pages/admin/index.tsx
+++ src/frontend/pages/admin/index.tsx
@@ -3,17 +3,14 @@ import { AdminPermission } from '../../_enums/adminPermission'
 import layout from '../../layouts/admin'
 import OrdersChart from '../../components/analytics/View/orders/chart'
 import RevenueChart from '../../components/analytics/View/revenue/chart'
-import WelcomeBack from '../../components/_common/WelcomeBack'
-import { authenticateUser } from '../../_utils/auth/authenticateUser'
+import AuthorizedPath from 'frontend/components/users/Authorization'
 
 const Page = (): ReactElement => {
-  if (!authenticateUser(AdminPermission.VIEW_ANALYTICS)) return <WelcomeBack />
-
   return (
-    <>
+    <AuthorizedPath permission={AdminPermission.VIEW_ANALYTICS}>
       <OrdersChart />
       <RevenueChart />
-    </>
+    </AuthorizedPath>
   )
 }
 
