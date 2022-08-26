1a2bc63d3629ec1c99effc73464f2cdc7244ba46
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-27 00:14:17 +0800

    Change authorization failure phrasing

--- src/frontend/components/_common/NoPermissions.tsx
+++ src/frontend/components/_common/NoPermissions.tsx
@@ -5,7 +5,7 @@ import AuthorizedPath from '../users/Authorization'
 const NoPermissions = (): ReactElement => {
   return (
     <AuthorizedPath>
-      <Typography>{'You have no authorization to access this area.'}</Typography>
+      <Typography>{'You are not authorized to access this area.'}</Typography>
     </AuthorizedPath>
   )
 }
