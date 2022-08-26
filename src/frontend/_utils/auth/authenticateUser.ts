25066e669d9a86b2ef7e2922059d204ec9403176
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-08-27 01:09:05 +0800

    Add checkAdminPermission function in authenticateUser

--- src/frontend/_utils/auth/authenticateUser.ts
+++ src/frontend/_utils/auth/authenticateUser.ts
@@ -3,23 +3,23 @@ import { GetUser } from '../../layouts/query'
 import { User } from '../../../types/user'
 import { AdminPermission } from '../../_enums/adminPermission'
 
-export const authenticateUser = (
+export const authenticateUser = (permission: AdminPermission): boolean => {
+  const { data, loading } = useQuery(GetUser)
+
+  if (loading) return
+
+  const user: User = data?.get_user
+
+  return checkAdminPermission(permission, user)
+}
+
+export const checkAdminPermission = (
   permission: AdminPermission,
   user: User
 ): boolean => {
-  let currentUser: User = user
-
-  if (!user) {
-    const { data, loading } = useQuery(GetUser, { skip: user !== null })
-
-    if (loading) return
-
-    currentUser = data?.get_user
-  }
-
   const adminPermission = Object(AdminPermission)[permission]
 
-  if (!currentUser?.permissions?.includes(adminPermission)) return false
+  if (!user?.permissions?.includes(adminPermission)) return false
 
   return true
 }
