7e18b222d5039c1fa525609badf12eb68560c70b
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-01 18:44:51 +0800

    Hotfix

--- src/backend/controllers/paymentMethods/mutations/update_payment_method.ts
+++ src/backend/controllers/paymentMethods/mutations/update_payment_method.ts
@@ -6,8 +6,8 @@ import {
 import { AdminPermission } from '../../../_enums/adminPermission'
 import { AuditLogAction } from '../../../_enums/auditLogAction'
 import { authenticateUser } from '../../../_utils/auth/authenticateUser'
+import { returnUpdatedData } from '../../../_utils/handleData/returnUpdatedData'
 import { createAuditLog } from '../../../_utils/handleData/createAuditLog'
-import { returnUpdatedData } from 'backend/_utils/handleData/returnUpdatedData'
 
 export default async (
   _root: undefined,
