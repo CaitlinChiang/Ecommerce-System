8538eb99f1d8abe9d8530b8e1570705397f5b6df
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-01 20:38:50 +0800

    Add main styling

--- src/frontend/themes/index.ts
+++ src/frontend/themes/index.ts
@@ -1,10 +1,87 @@
 import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import components from './Component'
+import typography from './Typography'
+import shadows from './Shadow'
 
-export default createTheme({
+const SidebarWidth = 265
+const TopbarHeight = 70
+
+const baseTheme = {
   palette: {
-    primary: { main: '#A88238' },
-    secondary: { main: '#794618' },
-    error: red
-  }
-})
+    primary: {
+      main: '#1a97f5',
+      light: '#e6f4ff',
+      dark: '#1682d4'
+    },
+    secondary: {
+      main: '#1e4db7',
+      light: '#ddebff',
+      dark: '#173f98'
+    },
+    success: {
+      main: '#00c292',
+      light: '#ebfaf2',
+      dark: '#00964b',
+      contrastText: '#ffffff'
+    },
+    danger: {
+      main: '#e46a76',
+      light: '#fdf3f5'
+    },
+    info: {
+      main: '#0bb2fb',
+      light: '#a7e3f4'
+    },
+    error: {
+      main: '#e46a76',
+      light: '#fdf3f5',
+      dark: '#e45a68'
+    },
+    warning: {
+      main: '#fec90f',
+      light: '#fff4e5',
+      dark: '#dcb014',
+      contrastText: '#ffffff'
+    },
+    text: {
+      secondary: '#777e89',
+      danger: '#fc4b6c'
+    },
+    grey: {
+      A100: '#ecf0f2',
+      A200: '#99abb4',
+      A400: '#767e89',
+      A700: '#e6f4ff'
+    },
+    action: {
+      disabledBackground: 'rgba(73,82,88,0.12)',
+      hoverOpacity: 0.02,
+      hover: 'rgba(0, 0, 0, 0.03)'
+    }
+  },
+  shape: {
+    borderRadius: 5
+  },
+  mixins: {
+    toolbar: {
+      color: '#949db2',
+      '@media(min-width:1280px)': {
+        minHeight: TopbarHeight,
+        padding: '0 30px'
+      },
+      '@media(max-width:1280px)': {
+        minHeight: '64px'
+      }
+    }
+  },
+  status: {
+    danger: '#e53e3e'
+  },
+  components,
+  typography,
+  shadows
+}
+
+const theme = createTheme(baseTheme)
+
+export { TopbarHeight, SidebarWidth, theme }
