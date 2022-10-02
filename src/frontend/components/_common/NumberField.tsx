e458c00f142a340f129aa70e3965b445474c5eb9
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-02 10:45:48 +0800

    Redesign number field

--- src/frontend/components/_common/NumberField.tsx
+++ src/frontend/components/_common/NumberField.tsx
@@ -1,55 +1,79 @@
 import { ReactElement } from 'react'
-import styles from '../../styles/_common/numberField'
 import { TextField } from '@mui/material'
-import { formatText } from '../../_utils/handleFormat/formatText'
+import { styled } from '@mui/material/styles'
+import CustomFormLabel from './CustomFormLabel'
 import { returnError } from '../../_utils/handleArgs/returnError'
 import { returnHelperText } from '../../_utils/handleArgs/returnHelperText'
+
+const StyledNumberField = styled(TextField)`
+  & .MuiOutlinedInput-input::-webkit-input-placeholder: {
+    color: '#767e89',
+    opacity: '1'
+  },
+  & .MuiOutlinedInput-notchedOutline: {
+    borderColor: '#dee3e9'
+  },
+  & .MuiOutlinedInput-input.Mui-disabled: {
+    backgroundColor: '#f8f9fb'
+  },
+  & .MuiOutlinedInput-input.Mui-disabled::-webkit-input-placeholder: {
+    color: '#767e89',
+    opacity: '1'
+  }
+`
 
 const NumberField = ({
   args,
   disabled,
   error,
+  fullWidth,
   label,
   nestedProp,
   required,
   setArgs,
-  targetProp,
-  width
+  targetProp
 }: {
   args: any
   disabled?: boolean
   error?: boolean
+  fullWidth?: boolean
   label?: string
   nestedProp?: string
   required?: boolean
   setArgs: React.Dispatch<React.SetStateAction<any>>
   targetProp: string
-  width?: number
 }): ReactElement => {
   const val = args[targetProp]
 
   return (
-    <TextField
-      disabled={disabled}
-      error={returnError({ args, error, targetProp, nestedProp })}
-      helperText={returnHelperText({ args, error, targetProp, nestedProp })}
-      label={label || formatText(nestedProp || targetProp)}
-      onChange={(e): void => {
-        const inputVal = e.target.value
+    <>
+      <CustomFormLabel
+        required={required}
+        text={label || nestedProp || targetProp}
+      />
+      <StyledNumberField
+        disabled={disabled}
+        error={returnError({ args, error, targetProp, nestedProp })}
+        fullWidth={fullWidth}
+        helperText={returnHelperText({ args, error, targetProp, nestedProp })}
+        onChange={(e): void => {
+          const inputVal = e.target.value
 
-        if (nestedProp) {
-          setArgs({ ...args, [targetProp]: { ...[val], [nestedProp]: inputVal } })
-        } else {
-          setArgs({ ...args, [targetProp]: inputVal })
-        }
-      }}
-      placeholder={'0'}
-      required={required}
-      sx={{ ...styles.textField, width: width || 300 }}
-      type={'number'}
-      value={!nestedProp ? val : val?.[nestedProp]}
-      InputProps={{ inputProps: { min: 0 } }}
-    />
+          if (nestedProp) {
+            setArgs({ ...args, [targetProp]: { ...[val], [nestedProp]: inputVal } })
+          } else {
+            setArgs({ ...args, [targetProp]: inputVal })
+          }
+        }}
+        placeholder={'0'}
+        required={required}
+        size={'small'}
+        type={'number'}
+        value={!nestedProp ? val : val?.[nestedProp]}
+        variant={'outlined'}
+        InputProps={{ inputProps: { min: 0 } }}
+      />
+    </>
   )
 }
 
