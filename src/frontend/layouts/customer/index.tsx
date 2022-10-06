d4d948cf4f0d0d1ba83c148f70784abf2c74c7b8
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-06 18:43:12 +0800

    Add styling to layout for customers

--- src/frontend/layouts/customer/index.tsx
+++ src/frontend/layouts/customer/index.tsx
@@ -1,16 +1,43 @@
 import { NextPage } from 'next'
-import { ReactElement, FunctionComponent } from 'react'
+import { ReactElement, FunctionComponent, useState, useEffect } from 'react'
 import { Box, Container, Typography } from '@mui/material'
 import { MainWrapper, PageWrapper } from '../common'
+import Header from './Header'
 import Navbar from './Navbar'
 import Footer from '../admin/Footer'
 
 export default (Page: FunctionComponent, { title }: { title?: string }) =>
   (): FunctionComponent | NextPage | ReactElement => {
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false)
+    const [width, setWidth] = useState<number>(0)
+
+    const handleWindowSizeChange = () => {
+      setWidth(window.innerWidth)
+    }
+
+    useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange)
+      return () => {
+        window.removeEventListener('resize', handleWindowSizeChange)
+      }
+    }, [])
+
+    const isMobile = true
+
     return (
       <>
         <MainWrapper>
-          <Navbar sx={{ backgroundColor: 'primary' }} />
+          <Header
+            isMobile={isMobile}
+            sx={{
+              paddingLeft: mobileOpen ? '265px' : '',
+              backgroundColor: 'primary'
+            }}
+            toggleMobileNavbar={() => setMobileOpen(true)}
+          />
+          {isMobile && (
+            <Navbar open={mobileOpen} onClose={() => setMobileOpen(false)} />
+          )}
           <PageWrapper>
             <Container maxWidth={false} sx={{ marginTop: 5 }}>
               <Box sx={{ minHeight: 'calc(100vh - 170px)' }}>
