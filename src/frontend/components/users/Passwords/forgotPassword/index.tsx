11bfb1d2b0f7da4832eebe27a118848567b9e094
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-08 20:36:48 +0800

    Redesign forgotPassword

--- src/frontend/components/users/Passwords/forgotPassword/index.tsx
+++ src/frontend/components/users/Passwords/forgotPassword/index.tsx
@@ -2,10 +2,11 @@ import { ReactElement, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useMutation } from '@apollo/client'
 import { ForgotUserPassword } from '../mutation'
-import { Button } from '@mui/material'
+import { Card, CardContent } from '@mui/material'
 import { ForgotPasswordArgs } from '../../../../../types/user'
 import { UserType } from '../../../../_enums/userType'
 import Text from '../../../_common/TextField'
+import MutationButton from '../../../_common/MutationButton'
 import { generateAdminUrl } from '../../../../_utils/auth/generateAdminUrl'
 
 const globalAny: any = global
@@ -33,24 +34,25 @@ const ForgotPassword = ({ type }: { type: UserType }): ReactElement => {
   )
 
   return (
-    <>
-      <Text
-        args={args}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-        targetProp={'email'}
-      />
-      <Button
-        disabled={forgotPasswordMutationState.loading}
-        onClick={(): void => {
-          setValidateFields(true)
-          forgotPasswordMutation()
-        }}
-      >
-        {'Get Verification Code'}
-      </Button>
-    </>
+    <Card>
+      <CardContent>
+        <Text
+          args={args}
+          error={validateFields}
+          required={true}
+          setArgs={setArgs}
+          targetProp={'email'}
+        />
+        <MutationButton
+          disabled={forgotPasswordMutationState.loading}
+          onClick={(): void => {
+            setValidateFields(true)
+            forgotPasswordMutation()
+          }}
+          title={'Get Verification Code'}
+        />
+      </CardContent>
+    </Card>
   )
 }
 
