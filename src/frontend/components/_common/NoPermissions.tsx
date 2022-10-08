ad6ed2e0455a57789ef4a9cc940ab03ad94c12e6
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-08 23:11:19 +0800

    Small changes to typo

--- src/frontend/components/_common/NoPermissions.tsx
+++ src/frontend/components/_common/NoPermissions.tsx
@@ -2,7 +2,11 @@ import { ReactElement } from 'react'
 import { Typography } from '@mui/material'
 
 const NoPermissions = (): ReactElement => {
-  return <Typography>{'You are not authorized to access this area.'}</Typography>
+  return (
+    <Typography variant={'h3'}>
+      {'You are not authorized to access this area.'}
+    </Typography>
+  )
 }
 
 export default NoPermissions
