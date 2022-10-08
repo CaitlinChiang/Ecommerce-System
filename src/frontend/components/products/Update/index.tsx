8eabcf7d470787b3ceb8b293762eadedc1cfa0cf
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-08 23:03:27 +0800

    Redesign update product

--- src/frontend/components/products/Update/index.tsx
+++ src/frontend/components/products/Update/index.tsx
@@ -2,16 +2,18 @@ import { ReactElement, useState, useEffect } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
 import { GetProduct } from '../View/query'
 import mutation from './mutation'
-import { Button, CircularProgress, Typography } from '@mui/material'
+import { Card, CardContent, CircularProgress, Typography } from '@mui/material'
 import { Product, UpdateProductArgs } from '../../../../types/product'
 import Text from '../../_common/TextField'
 import DatePickerField from '../../_common/DatePickerField'
 import CheckboxField from '../../_common/CheckboxField'
 import NumberField from '../../_common/NumberField'
 import ImageUploader from '../../_common/ImageUploader'
+import MutationButton from '../../_common/MutationButton'
 import ProductCategoriesSelect from '../../productCategories/View/select'
 import { correctArgs } from '../../../_utils/handleArgs/correctArgs'
 import { formatToPercentage } from '../../../_utils/handleFormat/formatToPercentage'
+import { formatToDecimal } from '../../../_utils/handleFormat/formatToDecimal'
 
 const globalAny: any = global
 
@@ -62,7 +64,7 @@ const UpdateProduct = ({
   }, [data])
 
   const [updateMutation, updateMutationState] = useMutation(mutation, {
-    variables: correctArgs(args),
+    variables: { ...correctArgs(args), discount: formatToDecimal(args?.discount) },
     onCompleted: () => {
       globalAny.setNotification(true, 'Product successfully updated!')
       refetch()
@@ -73,83 +75,97 @@ const UpdateProduct = ({
   return (
     <>
       {loading && <CircularProgress />}
-      <Typography>{`Created At: ${product?.createdAt}`}</Typography>
-      <Typography>{`Last Updated At: ${product?.updatedAt || '-'}`}</Typography>
-      <ProductCategoriesSelect
-        args={args}
-        disabled={disabled}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-      />
-      <Text
-        args={args}
-        disabled={disabled}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-        targetProp={'description'}
-      />
-      <DatePickerField
-        args={args}
-        disabled={disabled}
-        setArgs={setArgs}
-        targetProp={'expirationDate'}
-      />
-      <CheckboxField
-        args={args}
-        disabled={disabled}
-        setArgs={setArgs}
-        targetProp={'featured'}
-      />
-      <Text
-        args={args}
-        disabled={disabled}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-        targetProp={'name'}
-      />
-      <NumberField
-        args={args}
-        disabled={disabled}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-        targetProp={'price'}
-      />
-      <Text
-        args={args}
-        disabled={disabled}
-        placeholder={'ex. 20%'}
-        setArgs={setArgs}
-        targetProp={'discount'}
-      />
-      <CheckboxField args={args} setArgs={setArgs} targetProp={'showPublic'} />
-      <NumberField
-        args={args}
-        disabled={disabled}
-        error={validateFields}
-        required={true}
-        setArgs={setArgs}
-        targetProp={'stockQuantity'}
-      />
-      <ImageUploader
-        alt={'Product Photo'}
-        args={args}
-        disabled={disabled}
-        setArgs={setArgs}
-        targetProp={'image'}
-      />
-      <Button
-        disabled={disabled || updateMutationState.loading}
-        onClick={(): void => {
-          setValidateFields(true)
-          updateMutation()
-        }}
-      >
-        {'Save Changes'}
-      </Button>
+      <Card>
+        <CardContent>
+          <Typography
+            variant={'h4'}
+          >{`Created At: ${product?.createdAt}`}</Typography>
+          <Typography sx={{ marginBottom: 2 }} variant={'h4'}>{`Last Updated At: ${
+            product?.updatedAt || '-'
+          }`}</Typography>
+          <Text
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+            targetProp={'name'}
+          />
+          <ProductCategoriesSelect
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+          />
+          <NumberField
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+            targetProp={'price'}
+          />
+          <Text
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+            targetProp={'description'}
+          />
+          <DatePickerField
+            args={args}
+            disabled={disabled}
+            setArgs={setArgs}
+            targetProp={'expirationDate'}
+          />
+          <NumberField
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+            targetProp={'stockQuantity'}
+          />
+          <Text
+            args={args}
+            disabled={disabled}
+            placeholder={'ex. 20%'}
+            setArgs={setArgs}
+            targetProp={'discount'}
+          />
+          <CheckboxField
+            args={args}
+            disabled={disabled}
+            setArgs={setArgs}
+            targetProp={'showPublic'}
+          />
+          <CheckboxField
+            args={args}
+            disabled={disabled}
+            setArgs={setArgs}
+            targetProp={'featured'}
+          />
+          <ImageUploader
+            alt={'Product Photo'}
+            args={args}
+            disabled={disabled}
+            error={validateFields}
+            required={true}
+            setArgs={setArgs}
+            targetProp={'image'}
+          />
+          <MutationButton
+            disabled={disabled || updateMutationState.loading}
+            onClick={(): void => {
+              setValidateFields(true)
+              updateMutation()
+            }}
+            title={'Update'}
+          />
+        </CardContent>
+      </Card>
     </>
   )
 }
