03af5afe8b59e6e3f9826cf2ce192432403245d1
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-08 18:29:42 +0800

    Add CheckoutProductGridItem

--- src/frontend/components/_common/GridItem.tsx
+++ src/frontend/components/_common/GridItem.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
-import { Grid } from '@mui/material'
+import { Box, Divider, Grid, Typography } from '@mui/material'
 
-const GridItem = ({
+export const CartGridItem = ({
   content,
   xs,
   md,
@@ -27,4 +27,70 @@ const GridItem = ({
   )
 }
 
-export default GridItem
+export const CheckoutProductGridItem = ({
+  productName,
+  productVariantName,
+  quantity,
+  totalPrice
+}: {
+  productName: string
+  productVariantName?: string
+  quantity: string
+  totalPrice: string
+}): ReactElement => {
+  return (
+    <>
+      <Grid container>
+        <Grid item xs={6} md={6} lg={6}>
+          <Typography
+            sx={{ marginBottom: productVariantName ? 0 : 1 }}
+            variant={'h3'}
+          >
+            {productName}
+          </Typography>
+          <Typography color={'#707070'} sx={{ marginBottom: 1 }} variant={'h5'}>
+            {productVariantName}
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={6} lg={6}>
+          <Grid container justifyContent={'flex-end'}>
+            <Box>
+              <Typography sx={{ marginBottom: 1 }} variant={'h4'}>
+                {quantity}
+              </Typography>
+              <Typography sx={{ marginBottom: 3 }} variant={'h4'}>
+                {totalPrice}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Divider sx={{ marginBottom: 3 }} />
+    </>
+  )
+}
+
+export const CheckoutSummaryGridItem = ({
+  contentLeft,
+  contentRight
+}: {
+  contentLeft: string
+  contentRight: string
+}): ReactElement => {
+  return (
+    <Grid container>
+      <Grid item xs={6} md={6} lg={6}>
+        <Typography color={'#707070'} variant={'h5'}>
+          {contentLeft}
+        </Typography>
+      </Grid>
+      <Grid item xs={6} md={6} lg={6}>
+        <Grid container justifyContent={'flex-end'}>
+          <Typography color={'#707070'} variant={'h5'}>
+            {contentRight}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
