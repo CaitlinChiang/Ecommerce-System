f112fc07408419c34c58b920b2ec3a9470bb6dc8
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-08 18:30:11 +0800

    Redesign order success

--- src/frontend/components/orders/Create/orderSuccess.tsx
+++ src/frontend/components/orders/Create/orderSuccess.tsx
@@ -1,27 +1,43 @@
 import { ReactElement } from 'react'
 import { useRouter } from 'next/router'
-import { Button, Container, Link, Typography } from '@mui/material'
+import { Button, Card, CardContent, Grid, Link, Typography } from '@mui/material'
 
 const OrderSuccess = (): ReactElement => {
   const router = useRouter()
 
   return (
-    <Container>
-      <Typography>{'Order Successful!'}</Typography>
-      <Typography>
-        {
-          'Please check your email for the order receipt. You may also track your order '
-        }
-      </Typography>
-      <Link href={'/orders'}>{' here.'}</Link>
-      <Button
-        onClick={(): void => {
-          router.push('/')
-        }}
-      >
-        {'Return to Home'}
-      </Button>
-    </Container>
+    <Card sx={{ width: '90%', margin: 'auto' }}>
+      <CardContent>
+        <Grid
+          container
+          justifyContent={'center'}
+          alignItems={'center'}
+          direction={'column'}
+        >
+          <Grid item>
+            <Typography variant={'h2'}>{'Order Successful!'}</Typography>
+            <Typography sx={{ marginTop: 5, marginBottom: 2 }} variant={'h4'}>
+              {'Order receipt has been sent to your email.'}
+            </Typography>
+            <Link href={'/orders'}>
+              <Typography variant={'h4'}>
+                {'You may track your order here.'}
+              </Typography>
+            </Link>
+            <Button
+              color={'primary'}
+              onClick={(): void => {
+                router.push('/')
+              }}
+              sx={{ marginTop: 5, width: 300 }}
+              variant={'contained'}
+            >
+              {'Return to Home'}
+            </Button>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   )
 }
 
