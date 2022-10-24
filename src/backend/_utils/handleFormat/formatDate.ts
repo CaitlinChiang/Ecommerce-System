744dadaa44f0a65476f3aa4c049ca1917ad98e65
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-24 17:54:05 +0800

    Add display fallback

--- src/backend/_utils/handleFormat/formatDate.ts
+++ src/backend/_utils/handleFormat/formatDate.ts
@@ -1,7 +1,8 @@
 import { formatCurrentDate } from './formatCurrentDate'
 
-export const formatDate = (unformattedDate: Date): string => {
-  if (!unformattedDate) return
+export const formatDate = (unformattedDate: Date, display?: boolean): string => {
+  if (!unformattedDate && display === true) return '-'
+  if (!unformattedDate && display === false) return
 
   const dateString = unformattedDate.toLocaleString('en-US', { timeZone: 'UTC' })
   const splitDate = dateString.split(', ')
--- src/backend/_utils/handleFormat/formatDateTime.ts
+++ src/backend/_utils/handleFormat/formatDateTime.ts
@@ -1,7 +1,8 @@
 import { formatCurrentDate } from './formatCurrentDate'
 
-export const formatDateTime = (unformattedDate: Date): string => {
-  if (!unformattedDate) return
+export const formatDateTime = (unformattedDate: Date, display?: boolean): string => {
+  if (!unformattedDate && display === true) return '-'
+  if (!unformattedDate && display === false) return
 
   const dateString = unformattedDate.toLocaleString('en-US', { timeZone: 'UTC' })
   const splitDate = dateString.split(', ')
