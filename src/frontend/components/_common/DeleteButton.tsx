996851b70dd81d9252587f93c470fb62942abb8a
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2022-10-26 20:41:09 +0800

    Add type to DeleteButton mutation

--- src/frontend/components/_common/DeleteButton.tsx
+++ src/frontend/components/_common/DeleteButton.tsx
@@ -5,12 +5,14 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import { ObjectId } from 'mongodb'
 import { PaginateDataArgs } from '../../../types/actions/paginateData'
 import { RefetchDataArgs } from '../../../types/actions/refetchData'
+import { UserType } from '../../_enums/userType'
 import { refetchData } from '../../_utils/handleData/refetchData'
 
 const globalAny: any = global
 
 const DeleteButton = ({
   _id,
+  type,
   disabled,
   label,
   mutation,
@@ -18,14 +20,18 @@ const DeleteButton = ({
   setPaginateDataArgs
 }: {
   _id: ObjectId
+  type?: UserType
   disabled?: boolean
   label: string
   mutation: any
   refetchArgs: RefetchDataArgs
   setPaginateDataArgs: React.Dispatch<React.SetStateAction<PaginateDataArgs>>
 }): ReactElement => {
+  const mutationVariables: { _id: ObjectId; type?: UserType } = { _id }
+  if (type) mutationVariables.type = type
+
   const [deleteMutation, deleteMutationState] = useMutation(mutation, {
-    variables: { _id },
+    variables: mutationVariables,
     onCompleted: () => {
       globalAny.setNotification(true, `${label} has been successfully deleted.`)
 
