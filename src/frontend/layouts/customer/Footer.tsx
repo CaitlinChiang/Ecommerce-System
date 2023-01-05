4197c11ce5ce4fdbc0953ae9b02c93098518432f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-05 17:38:44 +0800

    Remove divider in footer

--- src/frontend/layouts/customer/Footer.tsx
+++ src/frontend/layouts/customer/Footer.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Box, Divider, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import ContactInformation from '../../components/websiteTexts/View/contactInformation'
 
 const Footer = () => {
   return (
     <Box sx={{ p: 3, textAlign: 'center' }}>
-      <Divider sx={{ marginBottom: 4 }} />
       <ContactInformation />
       <Typography sx={{ paddingTop: 3 }}>
         © 2022 All rights reserved by 'Company Name'
