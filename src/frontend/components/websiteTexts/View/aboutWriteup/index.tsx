79bfd28f110c7d8b661232d447bc191db137410f
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-01-05 17:38:19 +0800

    Design About

--- src/frontend/components/websiteTexts/View/aboutWriteup/index.tsx
+++ src/frontend/components/websiteTexts/View/aboutWriteup/index.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 import { useQuery } from '@apollo/client'
 import { GetWebsiteText } from '../query'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { WebsiteText } from '../../../../../types/websiteText'
 import { WebsiteTextType } from '../../../../_enums/websiteTextType'
 
@@ -13,8 +13,32 @@ const AboutWriteup = (): ReactElement => {
 
   return (
     <>
-      <Typography>{'About Company Name'}</Typography>
-      <Typography>{websiteText.content}</Typography>
+      <Box
+        style={{
+          backgroundImage: 'url(http://localhost:4000/images/about.jpg)',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          width: '100vw',
+          minHeight: '100vh'
+        }}
+      >
+        <Box style={{ marginTop: 25, paddingTop: '15%', paddingLeft: '5%' }}>
+          <Typography style={{ color: '#ffffff' }} variant={'h1'}>
+            {'About Company Name'}
+          </Typography>
+          <Typography
+            style={{
+              fontSize: 20,
+              marginTop: 20,
+              color: '#ffffff',
+              whiteSpace: 'pre-line'
+            }}
+          >
+            {websiteText.content}
+          </Typography>
+        </Box>
+      </Box>
     </>
   )
 }
